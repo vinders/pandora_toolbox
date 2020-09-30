e8ce265847025a71606d203007771b95b603ed38
vinders <vinders@users.noreply.github.com>
2020-09-30 19:10:13 +0200

    infra-scripts: install extern project dependencies (for win/linux/mac CI)

---
+++ _scripts/ci/install_dependencies.bat
@@ -0,0 +1,24 @@
+@echo off
+
+if not exist CMakeLists.txt cd ..
+if not exist CMakeLists.txt cd ..
+if not exist CMakeLists.txt (echo CMakeLists.txt not found in current directory nor in parent directory! && exit /B 1)
+
+:: gtest + libs directory
+if not exist ./_libs/gtest/CMakeLists.txt call git submodule update --init --recursive --remote
+cd _libs
+
+:: glew
+if not exist ./glew/CMakeLists.txt (
+    call git clone --depth 1 --branch "glew-cmake-2.1.0" https://github.com/Perlmint/glew-cmake glew
+)
+:: glm
+if not exist ./glm/CMakeLists.txt (
+    call git clone --depth 1 --branch "0.9.9.8" https://github.com/g-truc/glm glm
+)
+:: glfw3
+if not exist ./glfw3/CMakeLists.txt (
+    call git clone --depth 1 --branch "3.3.2" https://github.com/glfw/glfw glfw3
+)
+
+exit /B 0
---
+++ _scripts/ci/install_dependencies.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+if ! [ -e "./CMakeLists.txt" ]; then
+  cd ..
+  if ! [ -e "./CMakeLists.txt" ]; then
+    cd ..
+  fi
+fi
+
+if [ -z "$1" ]; then
+  PLATFORM="linux"
+else
+  PLATFORM="$1"
+fi
+
+# -- project dependencies --
+
+# gtest + libs directory
+if ! [ -e "./_libs/gtest/CMakeLists.txt" ]; then
+  git submodule update --init --recursive --remote
+fi
+cd "./_libs"
+
+# glew
+if ! [ -e "./glew/CMakeLists.txt" ]; then
+  if ! [ "$PLATFORM" = "android64" ]; then
+    if ! [ "$PLATFORM" = "ios" ]; then
+      git clone --depth 1 --branch "glew-cmake-2.1.0" https://github.com/Perlmint/glew-cmake glew
+    fi
+  fi
+fi
+
+# glm
+if ! [ -e "./glm/CMakeLists.txt" ]; then
+  git clone --depth 1 --branch "0.9.9.8" https://github.com/g-truc/glm glm
+fi
+
+# glfw3
+if ! [ -e "./glfw3/CMakeLists.txt" ]; then
+  git clone --depth 1 --branch "3.3.2" https://github.com/glfw/glfw glfw3
+fi
+
+exit 0
