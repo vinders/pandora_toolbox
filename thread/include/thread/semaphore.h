b4e2ab42eadc02eeb63220b328b2cdffad7a6584
vinders <vinders@users.noreply.github.com>
2020-10-02 12:06:39 +0200

    feat-thread: semaphore utility (producer/consumer pattern)

---
+++ thread/include/thread/semaphore.h
@@ -0,0 +1,175 @@
+/*******************************************************************************
+Author  :     Romain Vinders
+License :     MIT
+*******************************************************************************/
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <condition_variable>
+
+namespace pandora {
+  namespace thread {
+    /// @class Semaphore
+    /// @brief Synchronization primitive (for threads) with a counter, for producer/consumer patterns.
+    /// @details Semaphore for threads with an internal counter. Each call to wait() decrements it, and each call to notify() increments it.
+    ///          When the counter equals zero, any call to wait() will wait until notify() is called by another thread.
+    ///          Typically used for a producer/consumer pattern, or any other form of synchronization that requires a counter.
+    ///          When multiple threads are waiting for notifications, the order in which they'll awake is not guaranteed.
+    /// @warning To avoid deadlocks, the calls to wait() and notify() should never be in the same thread (unless notify() is called before wait()).
+    class Semaphore final {
+    public:
+      /// @brief Create instance with an initial count of 0
+      Semaphore() = default;
+      /// @brief Create instance with a custom initial count
+      Semaphore(uint32_t initialCount) noexcept : _count(initialCount) {}
+
+      Semaphore(const Semaphore&) = delete;
+      Semaphore(Semaphore&& rhs) = delete;
+      Semaphore& operator=(const Semaphore&) = delete;
+      Semaphore& operator=(Semaphore&& rhs) = delete;
+      /// @brief Awake all waiters and destroy instance
+      ~Semaphore() noexcept { 
+        std::unique_lock<std::mutex> lock(this->_lock);
+        while (this->_waiters) {
+          this->_count += this->_waiters;
+          lock.unlock();
+          this->_condition.notify_all();
+          std::this_thread::sleep_for(std::chrono::microseconds(1));
+          lock.lock();
+        }
+      }
+
+      // -- semaphore operations --
+
+      /// @brief Wait until the counter has a value above 0, then consume 1 unit
+      inline void wait() noexcept {
+        std::unique_lock<std::mutex> lock(this->_lock);
+        ++(this->_waiters);
+        while (this->_count == 0u)
+          this->_condition.wait(lock);
+        
+        --(this->_count);
+        --(this->_waiters);
+      }
+      /// @brief Wait for the counter to reach a specific count, then consume that count
+      inline void wait(uint32_t reqCount) noexcept {
+        std::unique_lock<std::mutex> lock(this->_lock);
+        this->_waiters += reqCount;
+        while (this->_count < reqCount)
+          this->_condition.wait(lock);
+        
+        this->_count -= reqCount;
+        this->_waiters -= reqCount;
+      }
+
+      /// @brief Check if the counter has a value above 0, and consume 1 unit if it's the case
+      /// @returns True on success, false if nothing to consume
+      inline bool tryWait() noexcept {
+        std::lock_guard<std::mutex> lock(this->_lock);
+        if (this->_count > 0u) {
+          --(this->_count);
+          return true;
+        }
+        return false;
+      }
+      /// @brief Check if the counter has at least a specific value, and consume that count if it's the case
+      /// @returns True on success, false if nothing to consume
+      inline bool tryWait(uint32_t reqCount) noexcept {
+        std::lock_guard<std::mutex> lock(this->_lock);
+        if (this->_count >= reqCount) {
+          this->_count -= reqCount;
+          return true;
+        }
+        return false;
+      }
+
+      /// @brief Increase the counter of 1 unit, then awake one waiting thread
+      inline void notify() noexcept {
+        std::unique_lock<std::mutex> lock(this->_lock);
+        ++(this->_count);
+        lock.unlock();
+
+        this->_condition.notify_one();
+      }
+      /// @brief Increase the counter of multiple units, then awake that number of waiting threads
+      inline void notify(uint32_t unitCount) noexcept {
+        std::unique_lock<std::mutex> lock(this->_lock);
+        this->_count += unitCount;
+        lock.unlock();
+
+        for (unitCount = this->_count; unitCount; --unitCount)
+          this->_condition.notify_one();
+      }
+      /// @brief Increase the counter to awake each waiting threads
+      inline void notifyAll() noexcept {
+        std::unique_lock<std::mutex> lock(this->_lock);
+        uint32_t unitCount = this->_waiters;
+        this->_count += unitCount;
+        lock.unlock();
+
+        this->_condition.notify_all();
+      }
+      
+      /// @brief Reset the counter to 0, and consume all units
+      /// @returns Value that the counter held before being reset
+      inline uint32_t reset() noexcept {
+        std::lock_guard<std::mutex> lock(this->_lock);
+        uint32_t unitCount = this->_count;
+        this->_count = 0u;
+        return unitCount;
+      }
+
+      // -- operations with timeout --
+
+      /// @brief Only wait until the counter has a value above 0 for a specific period of time
+      /// @returns True on success, false if timeout
+      template <typename _RepetitionType, typename _PeriodType>
+      inline bool tryWait(const std::chrono::duration<_RepetitionType, _PeriodType>& retryDuration) noexcept {
+        return tryWaitUntil(std::chrono::time_point<std::chrono::steady_clock>(std::chrono::steady_clock::now() + retryDuration));
+      }
+      /// @brief Only wait until the counter has reached a specific value for a specific period of time
+      /// @returns True on success, false if timeout
+      template <typename _RepetitionType, typename _PeriodType>
+      inline bool tryWait(uint32_t reqCount, const std::chrono::duration<_RepetitionType, _PeriodType>& retryDuration) noexcept {
+        return tryWaitUntil(reqCount, std::chrono::time_point<std::chrono::steady_clock>(std::chrono::steady_clock::now() + retryDuration));
+      }
+      template <typename _RepetitionType, typename _PeriodType>
+      inline bool try_wait_for(const std::chrono::duration<_RepetitionType, _PeriodType>& retryDuration) noexcept { return tryWait(retryDuration); } // STL-compliant version
+
+      /// @brief Wait until the counter has a value above 0, or until a specific time-point
+      /// @returns True on success, false if timeout
+      template <typename _ClockType, typename _DurationType>
+      inline bool tryWaitUntil(const std::chrono::time_point<_ClockType, _DurationType>& timeoutTimePoint) noexcept { return tryWaitUntil(1u, timeoutTimePoint); }
+      /// @brief Wait until the counter has reached a specific value, or until a specific time-point
+      /// @returns True on success, false if timeout
+      template <typename _ClockType, typename _DurationType>
+      bool tryWaitUntil(uint32_t reqCount, const std::chrono::time_point<_ClockType, _DurationType>& timeoutTimePoint) noexcept {
+        std::unique_lock<std::mutex> lock(this->_lock);
+        this->_waiters += reqCount;
+        while (this->_count < reqCount) {
+          if (this->_condition.wait_until(lock, timeoutTimePoint) == std::cv_status::timeout) {
+            this->_waiters -= reqCount;
+            return false;
+          }
+        }
+        this->_count -= reqCount;
+        this->_waiters -= reqCount;
+        return true;
+      }
+      template <typename _ClockType, typename _DurationType>
+      inline bool try_wait_until(const std::chrono::time_point<_ClockType, _DurationType>& timeoutTimePoint) noexcept { return tryWaitUntil(timeoutTimePoint); } // STL-compliant version
+            
+    private:
+      std::mutex _lock;
+      std::condition_variable _condition;
+
+      uint32_t _count{ 0u };
+      uint32_t _waiters{ 0u };
+    };
+    
+  }
+}
---
+++ thread/test/semaphore_test.cpp
@@ -0,0 +1,450 @@
+#include <gtest/gtest.h>
+#include <memory>
+#include <chrono>
+#include <future>
+#include <thread/semaphore.h>
+
+using namespace pandora::thread;
+
+class SemaphoreTest : public testing::Test {
+public:
+protected:
+  //static void SetUpTestCase() {}
+  //static void TearDownTestCase() {}
+
+  void SetUp() override {}
+  void TearDown() override {}
+};
+
+
+// -- semaphore operations --
+
+TEST_F(SemaphoreTest, waitNotifyOne) {
+  Semaphore sema;
+
+  EXPECT_FALSE(sema.tryWait());
+  EXPECT_FALSE(sema.tryWait(1u));
+  EXPECT_FALSE(sema.tryWait(2u));
+
+  sema.notify();
+  ASSERT_TRUE(sema.tryWait());
+  EXPECT_FALSE(sema.tryWait());
+  sema.notify();
+  sema.wait();
+  EXPECT_FALSE(sema.tryWait());
+
+  sema.notify();
+  ASSERT_TRUE(sema.tryWait(1u));
+  EXPECT_FALSE(sema.tryWait());
+  sema.notify();
+  sema.wait(1u);
+  EXPECT_FALSE(sema.tryWait());
+
+  sema.notify(1u);
+  ASSERT_TRUE(sema.tryWait());
+  EXPECT_FALSE(sema.tryWait());
+  sema.notify(1u);
+  sema.wait();
+  EXPECT_FALSE(sema.tryWait());
+}
+
+TEST_F(SemaphoreTest, waitNotifyCount) {
+  Semaphore sema;
+
+  sema.notify(2u);
+  EXPECT_TRUE(sema.tryWait());
+  ASSERT_TRUE(sema.tryWait());
+  EXPECT_FALSE(sema.tryWait());
+  sema.notify(2u);
+  sema.wait();
+  EXPECT_TRUE(sema.tryWait());
+  EXPECT_FALSE(sema.tryWait());
+  sema.notify(2u);
+  sema.wait();
+  sema.wait();
+  EXPECT_FALSE(sema.tryWait());
+
+  sema.notify(2u);
+  ASSERT_TRUE(sema.tryWait(2u));
+  EXPECT_FALSE(sema.tryWait());
+  sema.notify(2u);
+  sema.wait(2u);
+  EXPECT_FALSE(sema.tryWait());
+
+  sema.notify(2u);
+  EXPECT_FALSE(sema.tryWait(3u));
+  EXPECT_TRUE(sema.tryWait(2u));
+  EXPECT_FALSE(sema.tryWait());
+
+  sema.notify(3u);
+  ASSERT_TRUE(sema.tryWait(3u));
+  EXPECT_FALSE(sema.tryWait(2u));
+  EXPECT_FALSE(sema.tryWait());
+  sema.notify(3u);
+  sema.wait(3u);
+  EXPECT_FALSE(sema.tryWait(2u));
+  EXPECT_FALSE(sema.tryWait());
+
+  sema.notify(3u);
+  EXPECT_TRUE(sema.tryWait(2u));
+  ASSERT_TRUE(sema.tryWait());
+  EXPECT_FALSE(sema.tryWait());
+  sema.notify(3u);
+  sema.wait(2u);
+  sema.wait();
+  EXPECT_FALSE(sema.tryWait());
+
+  sema.notify(3u);
+  EXPECT_TRUE(sema.tryWait(1u));
+  ASSERT_TRUE(sema.tryWait(2u));
+  EXPECT_FALSE(sema.tryWait());
+  sema.notify(3u);
+  sema.wait(1u);
+  sema.wait(2u);
+  EXPECT_FALSE(sema.tryWait());
+}
+
+TEST_F(SemaphoreTest, initCtorWait) {
+  Semaphore sema1(1u);
+  EXPECT_FALSE(sema1.tryWait(2u));
+  EXPECT_TRUE(sema1.tryWait());
+  EXPECT_FALSE(sema1.tryWait());
+
+  Semaphore sema2(2u);
+  EXPECT_TRUE(sema2.tryWait());
+  EXPECT_TRUE(sema2.tryWait());
+  EXPECT_FALSE(sema2.tryWait());
+  Semaphore sema3(2u);
+  EXPECT_TRUE(sema3.tryWait(2u));
+  EXPECT_FALSE(sema3.tryWait());
+}
+
+TEST_F(SemaphoreTest, reset) {
+  Semaphore sema;
+  EXPECT_EQ(0u, sema.reset());
+  EXPECT_FALSE(sema.tryWait());
+
+  Semaphore sema1(1u);
+  EXPECT_EQ(1u, sema1.reset());
+  EXPECT_FALSE(sema1.tryWait());
+  EXPECT_EQ(0u, sema1.reset());
+
+  Semaphore sema2(2u);
+  EXPECT_TRUE(sema2.tryWait());
+  EXPECT_EQ(1u, sema2.reset());
+  EXPECT_FALSE(sema2.tryWait());
+  EXPECT_EQ(0u, sema2.reset());
+  Semaphore sema3(2u);
+  EXPECT_EQ(2u, sema3.reset());
+  EXPECT_FALSE(sema3.tryWait());
+  EXPECT_EQ(0u, sema3.reset());
+}
+
+TEST_F(SemaphoreTest, waitForDuration) {
+  Semaphore sema;
+
+  sema.notify();
+  EXPECT_TRUE(sema.tryWait(std::chrono::milliseconds(1)));
+  EXPECT_FALSE(sema.tryWait());
+  sema.notify();
+  EXPECT_TRUE(sema.try_wait_for(std::chrono::milliseconds(1)));
+  EXPECT_FALSE(sema.tryWait());
+  sema.notify();
+  EXPECT_TRUE(sema.tryWait(1u, std::chrono::milliseconds(1)));
+  EXPECT_FALSE(sema.tryWait());
+  sema.notify(2u);
+  EXPECT_TRUE(sema.tryWait(std::chrono::milliseconds(1)));
+  EXPECT_TRUE(sema.tryWait(std::chrono::milliseconds(1)));
+  EXPECT_FALSE(sema.tryWait());
+  sema.notify(2u);
+  EXPECT_TRUE(sema.tryWait(2u, std::chrono::milliseconds(1)));
+  EXPECT_FALSE(sema.tryWait());
+  sema.notify(2u);
+  EXPECT_TRUE(sema.tryWait(1u, std::chrono::milliseconds(1)));
+  EXPECT_TRUE(sema.tryWait(1u, std::chrono::milliseconds(1)));
+  EXPECT_FALSE(sema.tryWait());
+
+  EXPECT_FALSE(sema.tryWait(std::chrono::milliseconds(1)));
+
+  sema.notify(2u);
+  EXPECT_FALSE(sema.tryWait(3u, std::chrono::milliseconds(1)));
+  EXPECT_TRUE(sema.tryWait(1u, std::chrono::milliseconds(1)));
+  EXPECT_TRUE(sema.tryWait());
+}
+
+TEST_F(SemaphoreTest, waitUntilTimeout) {
+  Semaphore sema;
+
+  sema.notify();
+  EXPECT_TRUE(sema.tryWaitUntil(std::chrono::steady_clock::now() + std::chrono::milliseconds(1)));
+  EXPECT_FALSE(sema.tryWait());
+  sema.notify();
+  EXPECT_TRUE(sema.try_wait_until(std::chrono::steady_clock::now() + std::chrono::milliseconds(1)));
+  EXPECT_FALSE(sema.tryWait());
+  sema.notify();
+  EXPECT_TRUE(sema.tryWaitUntil(1u, std::chrono::steady_clock::now() + std::chrono::milliseconds(1)));
+  EXPECT_FALSE(sema.tryWait());
+  sema.notify(2u);
+  EXPECT_TRUE(sema.tryWaitUntil(std::chrono::steady_clock::now() + std::chrono::milliseconds(1)));
+  EXPECT_TRUE(sema.tryWaitUntil(std::chrono::steady_clock::now() + std::chrono::milliseconds(1)));
+  EXPECT_FALSE(sema.tryWait());
+  sema.notify(2u);
+  EXPECT_TRUE(sema.tryWaitUntil(2u, std::chrono::steady_clock::now() + std::chrono::milliseconds(1)));
+  EXPECT_FALSE(sema.tryWait());
+  sema.notify(2u);
+  EXPECT_TRUE(sema.tryWaitUntil(1u, std::chrono::steady_clock::now() + std::chrono::milliseconds(1)));
+  EXPECT_TRUE(sema.tryWaitUntil(1u, std::chrono::steady_clock::now() + std::chrono::milliseconds(1)));
+  EXPECT_FALSE(sema.tryWait());
+
+  auto timeout = std::chrono::steady_clock::now() + std::chrono::milliseconds(1);
+  EXPECT_FALSE(sema.tryWaitUntil(timeout));
+  EXPECT_TRUE(std::chrono::steady_clock::now() >= timeout);
+
+  sema.notify(2u);
+  EXPECT_FALSE(sema.tryWaitUntil(3u, std::chrono::steady_clock::now() + std::chrono::milliseconds(1)));
+  EXPECT_TRUE(sema.tryWaitUntil(1u, std::chrono::steady_clock::now() + std::chrono::milliseconds(1)));
+  EXPECT_TRUE(sema.tryWait());
+}
+
+
+// -- concurrency tests --
+
+TEST_F(SemaphoreTest, multiThreadWaitOne) {
+  bool isAsyncReady = false;
+  bool isAsyncSuccess = false;
+  Semaphore sema;
+
+  // other thread waiting first
+  isAsyncReady = false;
+  isAsyncSuccess = false;
+  auto promiseWait = std::async(std::launch::async, [&sema, &isAsyncReady, &isAsyncSuccess]() {
+    EXPECT_FALSE(sema.tryWait());
+    isAsyncReady = true;
+    sema.wait();
+    isAsyncSuccess = true;
+  });
+
+  do { std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+  } while (!isAsyncReady);
+  std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+  sema.notify();
+  ASSERT_TRUE(promiseWait.wait_for(std::chrono::milliseconds(2000LL)) != std::future_status::timeout);
+  EXPECT_TRUE(isAsyncSuccess);
+
+  // other thread waiting first for a duration
+  isAsyncReady = false;
+  isAsyncSuccess = false;
+  auto promiseWaitFor = std::async(std::launch::async, [&sema, &isAsyncReady, &isAsyncSuccess]() {
+    EXPECT_FALSE(sema.tryWait());
+    isAsyncReady = true;
+    EXPECT_TRUE(sema.tryWait(std::chrono::milliseconds(1000LL)));
+    isAsyncSuccess = true;
+    EXPECT_FALSE(sema.tryWait());
+  });
+
+  do { std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+  } while (!isAsyncReady);
+  std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+  sema.notify();
+  EXPECT_TRUE(promiseWaitFor.wait_for(std::chrono::milliseconds(2000LL)) != std::future_status::timeout);
+  EXPECT_TRUE(isAsyncSuccess);
+
+  // other thread waiting first until timeout
+  isAsyncReady = false;
+  isAsyncSuccess = false;
+  auto promiseWaitUntil = std::async(std::launch::async, [&sema, &isAsyncReady, &isAsyncSuccess]() {
+    EXPECT_FALSE(sema.tryWait());
+    isAsyncReady = true;
+    EXPECT_TRUE(sema.tryWaitUntil(std::chrono::steady_clock::now() + std::chrono::milliseconds(1000LL)));
+    isAsyncSuccess = true;
+    EXPECT_FALSE(sema.tryWait());
+  });
+
+  do { std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+  } while (!isAsyncReady);
+  std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+  sema.notify();
+  EXPECT_TRUE(promiseWaitUntil.wait_for(std::chrono::milliseconds(2000LL)) != std::future_status::timeout);
+  EXPECT_TRUE(isAsyncSuccess);
+}
+
+TEST_F(SemaphoreTest, multiThreadWaitCount) {
+  bool isAsyncReady = false;
+  bool isAsyncSuccess = false;
+  Semaphore sema;
+
+  // other thread waiting first
+  isAsyncReady = false;
+  isAsyncSuccess = false;
+  auto promiseWait = std::async(std::launch::async, [&sema, &isAsyncReady, &isAsyncSuccess]() {
+    EXPECT_FALSE(sema.tryWait());
+    isAsyncReady = true;
+    sema.wait(2u);
+    isAsyncSuccess = true;
+    EXPECT_TRUE(sema.tryWait());
+    EXPECT_FALSE(sema.tryWait());
+  });
+
+  do { std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+  } while (!isAsyncReady);
+  std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+  sema.notify(3u);
+  ASSERT_TRUE(promiseWait.wait_for(std::chrono::milliseconds(2000LL)) != std::future_status::timeout);
+  EXPECT_TRUE(isAsyncSuccess);
+
+  // other thread waiting first for a duration
+  isAsyncReady = false;
+  isAsyncSuccess = false;
+  auto promiseWaitFor = std::async(std::launch::async, [&sema, &isAsyncReady, &isAsyncSuccess]() {
+    EXPECT_FALSE(sema.tryWait());
+    isAsyncReady = true;
+    EXPECT_TRUE(sema.tryWait(2u, std::chrono::milliseconds(1000LL)));
+    isAsyncSuccess = true;
+    EXPECT_TRUE(sema.tryWait());
+    EXPECT_FALSE(sema.tryWait());
+    });
+
+  do { std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+  } while (!isAsyncReady);
+  std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+  sema.notify(3u);
+  EXPECT_TRUE(promiseWaitFor.wait_for(std::chrono::milliseconds(2000LL)) != std::future_status::timeout);
+  EXPECT_TRUE(isAsyncSuccess);
+
+  // other thread waiting first until timeout
+  isAsyncReady = false;
+  isAsyncSuccess = false;
+  auto promiseWaitUntil = std::async(std::launch::async, [&sema, &isAsyncReady, &isAsyncSuccess]() {
+    EXPECT_FALSE(sema.tryWait());
+    isAsyncReady = true;
+    EXPECT_TRUE(sema.tryWaitUntil(2u, std::chrono::steady_clock::now() + std::chrono::milliseconds(1000LL)));
+    isAsyncSuccess = true;
+    EXPECT_TRUE(sema.tryWait());
+    EXPECT_FALSE(sema.tryWait());
+    });
+
+  do { std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+  } while (!isAsyncReady);
+  std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+  sema.notify(3u);
+  EXPECT_TRUE(promiseWaitUntil.wait_for(std::chrono::milliseconds(2000LL)) != std::future_status::timeout);
+  EXPECT_TRUE(isAsyncSuccess);
+}
+
+TEST_F(SemaphoreTest, crossedWait) {
+  bool isAsyncReady = false;
+  Semaphore sema1; 
+  Semaphore sema2;
+
+  isAsyncReady = false;
+  auto promiseWait = std::async(std::launch::async, [&sema1, &sema2, &isAsyncReady]() {
+    EXPECT_FALSE(sema1.tryWait());
+    isAsyncReady = true;
+    EXPECT_TRUE(sema1.tryWaitUntil(std::chrono::steady_clock::now() + std::chrono::milliseconds(1000LL)));
+    EXPECT_FALSE(sema1.tryWait());
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+    sema2.notify();
+  });
+
+  do { std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+  } while (!isAsyncReady);
+  std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+  sema1.notify();
+  EXPECT_TRUE(sema2.tryWaitUntil(std::chrono::steady_clock::now() + std::chrono::milliseconds(1000LL)));
+  EXPECT_FALSE(sema2.tryWait());
+
+  EXPECT_TRUE(promiseWait.wait_for(std::chrono::milliseconds(2000LL)) != std::future_status::timeout);
+}
+
+TEST_F(SemaphoreTest, notifyAll) {
+  Semaphore sema;
+
+  bool isAsyncReady1 = false;
+  auto promiseWaitFor = std::async(std::launch::async, [&sema, &isAsyncReady1]() {
+    EXPECT_FALSE(sema.tryWait());
+    isAsyncReady1 = true;
+    EXPECT_TRUE(sema.tryWait(std::chrono::milliseconds(2000LL)));
+  });
+  bool isAsyncReady2 = false;
+  auto promiseWaitUntil = std::async(std::launch::async, [&sema, &isAsyncReady2]() {
+    EXPECT_FALSE(sema.tryWait());
+    isAsyncReady2 = true;
+    EXPECT_TRUE(sema.tryWaitUntil(2u, std::chrono::steady_clock::now() + std::chrono::milliseconds(2000LL)));
+  });
+
+  do { std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+  } while (!isAsyncReady1 || !isAsyncReady2);
+  std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+
+  sema.notifyAll();
+  EXPECT_TRUE(promiseWaitFor.wait_for(std::chrono::milliseconds(2500LL)) != std::future_status::timeout);
+  EXPECT_TRUE(promiseWaitUntil.wait_for(std::chrono::milliseconds(2500LL)) != std::future_status::timeout);
+}
+
+TEST_F(SemaphoreTest, destroyWhileThreadWaiting) {
+  std::unique_ptr<Semaphore> sema = std::make_unique<Semaphore>();
+
+  bool isAsyncReady = false;
+  auto promiseWaitFor = std::async(std::launch::async, [&sema, &isAsyncReady]() {
+    EXPECT_FALSE(sema->tryWait());
+    isAsyncReady = true;
+    EXPECT_TRUE(sema->tryWait(std::chrono::milliseconds(2000LL)));
+  });
+
+  do { std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+  } while (!isAsyncReady);
+  std::this_thread::sleep_for(std::chrono::milliseconds(2LL));
+
+  sema = nullptr;
+  EXPECT_TRUE(promiseWaitFor.wait_for(std::chrono::milliseconds(2500LL)) != std::future_status::timeout);
+}
+
+TEST_F(SemaphoreTest, orderVerification) {
+  bool isThread1Ready = false;
+  bool isThread2Ready = false;
+  bool isThread3Ready = false;
+  Semaphore sema;
+
+  auto promise1 = std::async(std::launch::async, [&sema, &isThread1Ready]() {
+    isThread1Ready = true;
+
+    EXPECT_TRUE(sema.tryWait(std::chrono::milliseconds(2000LL)));
+    sema.notify();
+  });
+
+  auto promise2 = std::async(std::launch::async, [&sema, &isThread1Ready, &isThread2Ready]() {
+    do { std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+    } while (!isThread1Ready);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+    isThread2Ready = true;
+
+    EXPECT_TRUE(sema.tryWait(std::chrono::milliseconds(2000LL)));
+    sema.notify();
+  });
+
+  auto promise3 = std::async(std::launch::async, [&sema, &isThread2Ready, &isThread3Ready]() {
+    do { std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+    } while (!isThread2Ready);
+    std::this_thread::sleep_for(std::chrono::milliseconds(2LL));
+    isThread3Ready = true;
+
+    EXPECT_TRUE(sema.tryWait(std::chrono::milliseconds(2000LL)));
+    sema.notify();
+  });
+
+  do { std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+  } while (!isThread1Ready || !isThread2Ready || !isThread3Ready);
+  std::this_thread::sleep_for(std::chrono::milliseconds(5LL));
+
+  sema.notify();
+
+  EXPECT_TRUE(promise1.wait_for(std::chrono::milliseconds(8000LL)) != std::future_status::timeout);
+  EXPECT_TRUE(promise2.wait_for(std::chrono::milliseconds(8000LL)) != std::future_status::timeout);
+  EXPECT_TRUE(promise3.wait_for(std::chrono::milliseconds(8000LL)) != std::future_status::timeout);
+  EXPECT_TRUE(sema.tryWait());
+  EXPECT_FALSE(sema.tryWait());
+
+  sema.notifyAll();
+  std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+}
