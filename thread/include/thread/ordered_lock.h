8e30a37adfe1d4796f9fe580fbbb64597751f997
vinders <vinders@users.noreply.github.com>
2020-10-02 12:07:20 +0200

    feat-thread: ordered lock (mutex with lock order)

---
+++ thread/include/thread/ordered_lock.h
@@ -0,0 +1,132 @@
+/*******************************************************************************
+Author  :     Romain Vinders
+License :     MIT
+*******************************************************************************/
+#pragma once
+
+#include <mutex>
+#include <deque>
+#include <condition_variable>
+
+namespace pandora {
+  namespace thread {
+    /// @class OrderedLock
+    /// @brief Synchronization primitive to protect data from being simultaneously accessed, with guaranteed FIFO order.
+    /// @description Synchronization primitive to protect data from being simultaneously accessed by multiple threads.
+    ///              Acts like a standard mutex, except that the order of lock is guaranteed (FIFO).
+    class OrderedLock final {
+      using InternalTicket = std::condition_variable*;
+    public:
+      /// @brief Create an new unlocked instance
+      OrderedLock() = default;
+      OrderedLock(const OrderedLock&) = delete;
+      OrderedLock(OrderedLock&&) = delete;
+      OrderedLock& operator=(const OrderedLock&) = delete;
+      OrderedLock& operator=(OrderedLock&&) = delete;
+
+      // -- lock management --
+
+      /// @brief Wait until object unlocked, then lock it
+      inline void lock() noexcept {
+        std::unique_lock<std::mutex> lock(this->_lock);
+        if (!this->_isLocked) {
+          this->_isLocked = true;
+        }
+        else {
+          std::condition_variable condition;
+          this->_orderedTickets.emplace_back(&condition);
+          while (this->_notifiedTicket != &condition) // if awaken by other system signal, ignore it
+            condition.wait(lock);
+          this->_notifiedTicket = InternalTicket{ nullptr };
+        }
+      }
+
+      /// @brief Only lock the object if there's no need to wait (already unlocked)
+      /// @returns True on lock success, false if already locked by another owner
+      inline bool tryLock() noexcept {
+        std::lock_guard<std::mutex> lock(this->_lock);
+        if (!this->_isLocked) {
+          this->_isLocked = true;
+          return true;
+        }
+        return false;
+      }
+      inline bool try_lock() noexcept { return tryLock(); } // STL-compliant version
+
+      /// @brief Unlock the object
+      /// @warning Should only be called after having called lock() (by the thread that currently owns the object).
+      inline void unlock() noexcept {
+        std::lock_guard<std::mutex> lock(this->_lock);
+        if (this->_orderedTickets.empty()) {
+          this->_isLocked = false;
+        }
+        else {
+          this->_notifiedTicket = this->_orderedTickets.front();
+          this->_orderedTickets.pop_front();
+          this->_notifiedTicket->notify_one();
+        }
+      }
+      
+      // -- lock with timeout --
+
+      /// @brief Only wait for a specific period for the object to be unlocked.
+      /// @returns True on lock success, false if timeout
+      template <typename _RepetitionType, typename _PeriodType>
+      inline bool tryLock(const std::chrono::duration<_RepetitionType, _PeriodType>& retryDuration) noexcept {
+        return tryLockUntil(std::chrono::time_point<std::chrono::steady_clock>(std::chrono::steady_clock::now() + retryDuration));
+      }
+      template <typename _RepetitionType, typename _PeriodType>
+      inline bool try_lock_for(const std::chrono::duration<_RepetitionType, _PeriodType>& retryDuration) noexcept { return tryLock(retryDuration); } // STL-compliant version
+
+      /// @brief Only wait until a specific time-point for the object to be unlocked.
+      /// @returns True on lock success, false if timeout
+      template <typename _ClockType, typename _DurationType>
+      bool tryLockUntil(const std::chrono::time_point<_ClockType, _DurationType>& timeoutTimePoint) noexcept {
+        std::unique_lock<std::mutex> lock(this->_lock);
+        if (!this->_isLocked) {
+          this->_isLocked = true;
+        }
+        else {
+          std::condition_variable condition;
+          this->_orderedTickets.emplace_back(&condition);
+          while (this->_notifiedTicket != &condition) { // if awaken by other system signal, ignore it
+            if (condition.wait_until(lock, timeoutTimePoint) == std::cv_status::timeout && this->_notifiedTicket != &condition) {
+              _cancelPendingTicket(&condition);
+              return false;
+            }
+          }
+          this->_notifiedTicket = InternalTicket{ nullptr };
+        }
+        return true;
+      }
+      template <typename _ClockType, typename _DurationType>
+      inline bool try_lock_until(const std::chrono::time_point<_ClockType, _DurationType>& timeoutTimePoint) noexcept { return tryLockUntil(timeoutTimePoint); } // STL-compliant version
+      
+      // -- lock status --
+      
+      /// @brief Check whether the OrderedLock is locked or not
+      inline bool isLocked() const noexcept { std::unique_lock<std::mutex> lock(this->_lock); return this->_isLocked; }
+      /// @brief Get number of threads currently waiting for the lock
+      inline size_t queueSize() const noexcept { std::unique_lock<std::mutex> lock(this->_lock); return this->_orderedTickets.size(); }
+      
+    private:
+      // -- pending ticket management --
+      
+      void _cancelPendingTicket(InternalTicket ticket) noexcept {
+        for (auto it = this->_orderedTickets.begin(); it != this->_orderedTickets.end(); ++it) {
+          if (*it == ticket) {
+            this->_orderedTickets.erase(it);
+            break;
+          }
+        }
+      }
+      
+    private:
+      mutable std::mutex _lock;
+      std::deque<InternalTicket> _orderedTickets;
+      InternalTicket _notifiedTicket{ nullptr };
+      bool _isLocked = false;
+    };
+
+  }
+}
---
+++ thread/test/ordered_lock_test.cpp
@@ -0,0 +1,257 @@
+#include <gtest/gtest.h>
+#include <thread>
+#include <mutex>
+#include <future>
+#include <thread/ordered_lock.h>
+
+using namespace pandora::thread;
+
+class OrderedLockTest : public testing::Test {
+public:
+protected:
+  //static void SetUpTestCase() {}
+  //static void TearDownTestCase() {}
+
+  void SetUp() override {}
+  void TearDown() override {}
+};
+
+
+// -- lock operations --
+
+TEST_F(OrderedLockTest, lockUnlockedInstance) {
+  OrderedLock mutex;
+
+  mutex.lock();
+  EXPECT_FALSE(mutex.tryLock());
+  mutex.unlock();
+
+  EXPECT_TRUE(mutex.tryLock());
+  EXPECT_FALSE(mutex.tryLock());
+  mutex.unlock();
+  EXPECT_TRUE(mutex.try_lock());
+  EXPECT_FALSE(mutex.tryLock());
+  mutex.unlock();
+
+  EXPECT_TRUE(mutex.tryLock(std::chrono::milliseconds(1)));
+  EXPECT_FALSE(mutex.tryLock());
+  mutex.unlock();
+  EXPECT_TRUE(mutex.try_lock_for(std::chrono::milliseconds(1)));
+  EXPECT_FALSE(mutex.tryLock());
+  mutex.unlock();
+
+  EXPECT_TRUE(mutex.tryLockUntil(std::chrono::steady_clock::now() + std::chrono::milliseconds(1)));
+  EXPECT_FALSE(mutex.tryLock());
+  mutex.unlock();
+  EXPECT_TRUE(mutex.try_lock_until(std::chrono::steady_clock::now() + std::chrono::milliseconds(1)));
+  EXPECT_FALSE(mutex.tryLock());
+  mutex.unlock();
+}
+
+TEST_F(OrderedLockTest, lockGuard) {
+  OrderedLock mutex;
+  { // scope
+    std::lock_guard<OrderedLock> lock(mutex);
+    EXPECT_FALSE(mutex.tryLock());
+  } // end of scope
+
+  EXPECT_TRUE(mutex.tryLock());
+}
+
+TEST_F(OrderedLockTest, tryLockFailure) {
+  OrderedLock mutex;
+  std::lock_guard<OrderedLock> lock(mutex);
+
+  EXPECT_FALSE(mutex.tryLock());
+  EXPECT_FALSE(mutex.try_lock());
+
+  EXPECT_FALSE(mutex.tryLock(std::chrono::milliseconds(1)));
+  EXPECT_FALSE(mutex.try_lock_for(std::chrono::microseconds(1)));
+
+  auto timeout = std::chrono::steady_clock::now() + std::chrono::milliseconds(1);
+  EXPECT_FALSE(mutex.tryLockUntil(timeout));
+  EXPECT_TRUE(std::chrono::steady_clock::now() >= timeout);
+
+  timeout = std::chrono::steady_clock::now() + std::chrono::microseconds(1);
+  EXPECT_FALSE(mutex.try_lock_until(timeout));
+  EXPECT_TRUE(std::chrono::steady_clock::now() >= timeout);
+}
+
+
+// -- concurrency test --
+
+TEST_F(OrderedLockTest, multiThreadLock) {
+  bool isAsyncReady = false;
+  OrderedLock mutex;
+
+  // lock waiting for other thread with timeout duration
+  mutex.lock();
+  isAsyncReady = false;
+  auto promise = std::async(std::launch::async, [&mutex, &isAsyncReady]() {
+    bool isSuccess = mutex.tryLock();
+    EXPECT_FALSE(isSuccess);
+    isAsyncReady = true;
+
+    if (!isSuccess) {
+      EXPECT_TRUE(mutex.tryLock(std::chrono::milliseconds(2000LL)));
+    }
+    mutex.unlock();
+    });
+
+  do { std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+  } while (!isAsyncReady);
+  std::this_thread::sleep_for(std::chrono::milliseconds(2LL));
+  mutex.unlock();
+  promise.get();
+
+  // lock waiting for other thread until timeout time-point
+  EXPECT_TRUE(mutex.tryLock());
+  isAsyncReady = false;
+  auto promiseUntil = std::async(std::launch::async, [&mutex, &isAsyncReady]() {
+    bool isSuccess = mutex.tryLock();
+    EXPECT_FALSE(isSuccess);
+    isAsyncReady = true;
+
+    if (!isSuccess) {
+      EXPECT_TRUE(mutex.tryLockUntil(std::chrono::steady_clock::now() + std::chrono::milliseconds(2000LL)));
+    }
+    mutex.unlock();
+    });
+
+  do { std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+  } while (!isAsyncReady);
+  std::this_thread::sleep_for(std::chrono::milliseconds(2LL));
+  mutex.unlock();
+  promiseUntil.get();
+
+  // basic lock wait
+  EXPECT_TRUE(mutex.tryLock());
+  isAsyncReady = false;
+  auto promiseBase = std::async(std::launch::async, [&mutex, &isAsyncReady]() {
+    isAsyncReady = true;
+    mutex.lock();
+    mutex.unlock();
+    });
+
+  do { std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+  } while (!isAsyncReady);
+  std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+  mutex.unlock();
+  EXPECT_TRUE(promiseBase.wait_for(std::chrono::milliseconds(2000LL)) != std::future_status::timeout);
+}
+
+// -- lock order --
+
+TEST_F(OrderedLockTest, orderVerification) {
+  bool isThread1Ready = false;
+  bool isThread2Ready = false;
+  bool isThread3Ready = false;
+  std::vector<int> lockOrder;
+  OrderedLock mutex;
+
+  mutex.lock();
+
+  auto promise1 = std::async(std::launch::async, [&mutex, &lockOrder , &isThread1Ready]() {
+    isThread1Ready = true;
+
+    bool isSuccess = mutex.tryLock(std::chrono::milliseconds(1000LL));
+    lockOrder.push_back(1);
+    EXPECT_TRUE(isSuccess);
+    if (isSuccess)
+      mutex.unlock();
+  });
+
+  auto promise2 = std::async(std::launch::async, [&mutex, &lockOrder, &isThread1Ready, &isThread2Ready]() {
+    do { std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+    } while (!isThread1Ready);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+    isThread2Ready = true;
+
+    bool isSuccess = mutex.tryLock(std::chrono::milliseconds(1000LL));
+    lockOrder.push_back(2);
+    EXPECT_TRUE(isSuccess);
+    if (isSuccess)
+      mutex.unlock();
+  });
+
+  auto promise3 = std::async(std::launch::async, [&mutex, &lockOrder, &isThread2Ready, &isThread3Ready]() {
+    do { std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+    } while (!isThread2Ready);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+    isThread3Ready = true;
+
+    bool isSuccess = mutex.tryLock(std::chrono::milliseconds(1000LL));
+    lockOrder.push_back(3);
+    EXPECT_TRUE(isSuccess);
+    if (isSuccess)
+      mutex.unlock();
+  });
+
+  do { std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+  } while (!isThread1Ready || !isThread2Ready || !isThread3Ready);
+  std::this_thread::sleep_for(std::chrono::milliseconds(2LL));
+  mutex.unlock();
+
+  EXPECT_TRUE(promise1.wait_for(std::chrono::milliseconds(2000LL)) != std::future_status::timeout);
+  EXPECT_TRUE(promise2.wait_for(std::chrono::milliseconds(2000LL)) != std::future_status::timeout);
+  EXPECT_TRUE(promise3.wait_for(std::chrono::milliseconds(2000LL)) != std::future_status::timeout);
+  EXPECT_TRUE(lockOrder.size() == 3 && lockOrder[0] == 1 && lockOrder[1] == 2 && lockOrder[2] == 3);
+  EXPECT_TRUE(mutex.tryLock());
+}
+
+TEST_F(OrderedLockTest, orderVerificationReversed) {
+  bool isThread1Ready = false;
+  bool isThread2Ready = false;
+  bool isThread3Ready = false;
+  std::vector<int> lockOrder;
+  OrderedLock mutex;
+
+  mutex.lock();
+
+  auto promise1 = std::async(std::launch::async, [&mutex, &lockOrder, &isThread1Ready, &isThread2Ready]() {
+    do { std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+    } while (!isThread2Ready);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+    isThread1Ready = true;
+
+    bool isSuccess = mutex.tryLock(std::chrono::milliseconds(1000LL));
+    lockOrder.push_back(1);
+    EXPECT_TRUE(isSuccess);
+    if (isSuccess)
+      mutex.unlock();
+  });
+
+  auto promise2 = std::async(std::launch::async, [&mutex, &lockOrder, &isThread2Ready, &isThread3Ready]() {
+    do { std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+    } while (!isThread3Ready);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+    isThread2Ready = true;
+
+    bool isSuccess = mutex.tryLock(std::chrono::milliseconds(1000LL));
+    lockOrder.push_back(2);
+    EXPECT_TRUE(isSuccess);
+    if (isSuccess)
+      mutex.unlock();
+  });
+
+  auto promise3 = std::async(std::launch::async, [&mutex, &lockOrder, &isThread3Ready]() {
+    isThread3Ready = true;
+
+    bool isSuccess = mutex.tryLock(std::chrono::milliseconds(1000LL));
+    lockOrder.push_back(3);
+    EXPECT_TRUE(isSuccess);
+    if (isSuccess)
+      mutex.unlock();
+    });
+
+  do { std::this_thread::sleep_for(std::chrono::milliseconds(1LL));
+  } while (!isThread1Ready || !isThread2Ready);
+  std::this_thread::sleep_for(std::chrono::milliseconds(2LL));
+  mutex.unlock();
+
+  EXPECT_TRUE(promise1.wait_for(std::chrono::milliseconds(2000LL)) != std::future_status::timeout);
+  EXPECT_TRUE(promise2.wait_for(std::chrono::milliseconds(2000LL)) != std::future_status::timeout);
+  EXPECT_TRUE(promise3.wait_for(std::chrono::milliseconds(2000LL)) != std::future_status::timeout);
+  EXPECT_TRUE(lockOrder.size() == 3 && lockOrder[0] == 3 && lockOrder[1] == 2 && lockOrder[2] == 1);
+  EXPECT_TRUE(mutex.tryLock());
+}
