f7b9f9718500e7ef5357db727315c374a0bd08b9
vinders <vinders@users.noreply.github.com>
2020-10-03 13:42:59 +0200

    feat-pattern: custom type traits (copy/move/compare/iterator) + copy/move enablers

---
+++ pattern/include/pattern/class_traits.h
@@ -0,0 +1,217 @@
+/*******************************************************************************
+Author  :     Romain Vinders
+License :     MIT
+--------------------------------------------------------------------------------
+Custom type traits for classes
+*******************************************************************************/
+#pragma once
+
+#include <cstddef>
+#include <type_traits>
+#include <iterator>
+
+namespace pandora {
+  namespace pattern {
+    // -- traits - comparisons --
+
+    /// @brief Verify if a class contains the equality operator (==)
+    template <typename _Type, typename _RhsType>
+    struct has_operator_equals {
+      template <typename _Left = _Type, typename _Right = _RhsType> static auto test(_Left&& lhs, _Right&& rhs) -> decltype(lhs == rhs, void(), std::true_type{});
+      static auto test(...) -> std::false_type;
+      using type = decltype(test(std::declval<_Type>(), std::declval<_RhsType>()));
+    };
+    /// @brief Verify if a class contains the 'less' operator (<)
+    template <typename _Type, typename _RhsType>
+    struct has_operator_less {
+      template <typename _Left = _Type, typename _Right = _RhsType> static auto test(_Left&& lhs, _Right&& rhs) -> decltype(lhs < rhs, void(), std::true_type{});
+      static auto test(...)->std::false_type;
+      using type = decltype(test(std::declval<_Type>(), std::declval<_RhsType>()));
+    };
+    /// @brief Verify if a class contains the 'less or equal' operator (<=)
+    template <typename _Type, typename _RhsType>
+    struct has_operator_less_eq {
+      template <typename _Left = _Type, typename _Right = _RhsType> static auto test(_Left&& lhs, _Right&& rhs) -> decltype(lhs <= rhs, void(), std::true_type{});
+      static auto test(...)->std::false_type;
+      using type = decltype(test(std::declval<_Type>(), std::declval<_RhsType>()));
+    };
+    /// @brief Verify if a class contains the 'greater' operator (>)
+    template <typename _Type, typename _RhsType>
+    struct has_operator_greater {
+      template <typename _Left = _Type, typename _Right = _RhsType> static auto test(_Left&& lhs, _Right&& rhs) -> decltype(lhs > rhs, void(), std::true_type{});
+      static auto test(...)->std::false_type;
+      using type = decltype(test(std::declval<_Type>(), std::declval<_RhsType>()));
+    };
+    /// @brief Verify if a class contains the 'greater or equal' operator (>=)
+    template <typename _Type, typename _RhsType>
+    struct has_operator_greater_eq {
+      template <typename _Left = _Type, typename _Right = _RhsType> static auto test(_Left&& lhs, _Right&& rhs) -> decltype(lhs >= rhs, void(), std::true_type{});
+      static auto test(...)->std::false_type;
+      using type = decltype(test(std::declval<_Type>(), std::declval<_RhsType>()));
+    };
+
+
+    // -- traits - iterators --
+
+    /// @brief Verify if a class is an input iterator
+    template <typename _Iterator>
+    using is_input_iterator = typename std::is_convertible<typename std::iterator_traits<_Iterator>::iterator_category, std::input_iterator_tag>;
+    /// @brief Verify if a class is an output iterator
+    template <typename _Iterator>
+    using is_output_iterator = typename std::is_convertible<typename std::iterator_traits<_Iterator>::iterator_category, std::output_iterator_tag>;
+    /// @brief Verify if a class is a forward iterator
+    template <typename _Iterator>
+    using is_forward_iterator = typename std::is_convertible<typename std::iterator_traits<_Iterator>::iterator_category, std::forward_iterator_tag>;
+    /// @brief Verify if a class is a bidirectional iterator
+    template <typename _Iterator>
+    using is_bidirectional_iterator = typename std::is_convertible<typename std::iterator_traits<_Iterator>::iterator_category, std::bidirectional_iterator_tag>;
+    /// @brief Verify if a class is a random access iterator
+    template <typename _Iterator>
+    using is_random_access_iterator = typename std::is_convertible<typename std::iterator_traits<_Iterator>::iterator_category, std::random_access_iterator_tag>;
+
+
+    // -- conditional operators - copy / move --
+
+    /// @brief Enable if a type is copy constructible
+    template <typename _DataType, typename _EnabledType = void>
+    using enable_if_copy_constructible = typename std::enable_if<!std::is_class<_DataType>::value || std::is_copy_constructible<_DataType>::value, _EnabledType>::type;
+    /// @brief Enable if a type is copy assignable
+    template <typename _DataType, typename _EnabledType = void>
+    using enable_if_copy_assignable = typename std::enable_if<!std::is_class<_DataType>::value || std::is_copy_assignable<_DataType>::value, _EnabledType>::type;
+
+    /// @brief Enable if a type is move constructible
+    template <typename _DataType, typename _EnabledType = void>
+    using enable_if_move_constructible = typename std::enable_if<!std::is_class<_DataType>::value || std::is_move_constructible<_DataType>::value, _EnabledType>::type;
+    /// @brief Enable if a type is move assignable
+    template <typename _DataType, typename _EnabledType = void>
+    using enable_if_move_assignable = typename std::enable_if<!std::is_class<_DataType>::value || std::is_move_assignable<_DataType>::value, _EnabledType>::type;
+
+    /// @brief Enable if a type is copy constructible and not move constructible
+    template <typename _DataType, typename _EnabledType = void>
+    using enable_if_copy_or_move_constructible = typename std::enable_if<!std::is_class<_DataType>::value || std::is_copy_constructible<_DataType>::value || std::is_move_constructible<_DataType>::value, _EnabledType>::type;
+    /// @brief Enable if a type is copy assignable and not move assignable
+    template <typename _DataType, typename _EnabledType = void>
+    using enable_if_copy_or_move_assignable = typename std::enable_if<!std::is_class<_DataType>::value || std::is_copy_assignable<_DataType>::value || std::is_move_assignable<_DataType>::value, _EnabledType>::type;
+
+    /// @brief Enable if a type is copy constructible and not move constructible
+    template <typename _DataType, typename _EnabledType = void>
+    using enable_if_copy_not_move_constructible = typename std::enable_if<!std::is_class<_DataType>::value || (std::is_copy_constructible<_DataType>::value && !std::is_move_constructible<_DataType>::value), _EnabledType>::type;
+    /// @brief Enable if a type is copy assignable and not move assignable
+    template <typename _DataType, typename _EnabledType = void>
+    using enable_if_copy_not_move_assignable = typename std::enable_if<!std::is_class<_DataType>::value || (std::is_copy_assignable<_DataType>::value && !std::is_move_assignable<_DataType>::value), _EnabledType>::type;
+
+
+    // -- conditional operators - comparisons --
+
+    /// @brief Enable if a type has the equality operator (==)
+    template <typename _DataType, typename _EnabledType = void>
+    using enable_if_operator_equals = typename std::enable_if<!std::is_class<_DataType>::value || has_operator_equals<_DataType,_DataType>::type::value, _EnabledType>::type;
+    /// @brief Enable if a type doesn't have the equality operator (==)
+    template <typename _DataType, typename _EnabledType = void>
+    using enable_if_not_operator_equals = typename std::enable_if<!std::is_class<_DataType>::value && !has_operator_equals<_DataType, _DataType>::type::value, _EnabledType>::type;
+
+    /// @brief Enable if a type has the 'less' operator (<)
+    template <typename _DataType, typename _EnabledType = void>
+    using enable_if_operator_less = typename std::enable_if<!std::is_class<_DataType>::value || has_operator_less<_DataType, _DataType>::type::value, _EnabledType>::type;
+    /// @brief Enable if a type has the 'less or equal' operator (<=)
+    template <typename _DataType, typename _EnabledType = void>
+    using enable_if_operator_less_eq = typename std::enable_if<!std::is_class<_DataType>::value || has_operator_less_eq<_DataType, _DataType>::type::value, _EnabledType>::type;
+    /// @brief Enable if a type has the 'greater' operator (>)
+    template <typename _DataType, typename _EnabledType = void>
+    using enable_if_operator_greater = typename std::enable_if<!std::is_class<_DataType>::value || has_operator_greater<_DataType, _DataType>::type::value, _EnabledType>::type;
+    /// @brief Enable if a type has the greater or equal' operator (>=)
+    template <typename _DataType, typename _EnabledType = void>
+    using enable_if_operator_greater_eq = typename std::enable_if<!std::is_class<_DataType>::value || has_operator_greater_eq<_DataType, _DataType>::type::value, _EnabledType>::type;
+
+
+    // -- conditional operators - iterators --
+
+    /// @brief Enable if a type is a valid iterator
+    template <typename _Iterator, typename _EnabledType = void>
+    using enable_if_iterator = typename std::enable_if<is_input_iterator<_Iterator>::value
+                                                    || is_output_iterator<_Iterator>::value
+                                                    || is_forward_iterator<_Iterator>::value
+                                                    || is_bidirectional_iterator<_Iterator>::value
+                                                    || is_random_access_iterator<_Iterator>::value, _EnabledType>::type;
+    /// @brief Enable if a type is a valid iterator that can be used bi-directionally
+    template <typename _Iterator, typename _EnabledType = void>
+    using enable_if_iterator_reversible = typename std::enable_if<std::is_base_of<std::bidirectional_iterator_tag, typename std::iterator_traits<_Iterator>::iterator_category>::value
+                                                               || is_bidirectional_iterator<_Iterator>::value
+                                                               || is_random_access_iterator<_Iterator>::value, _EnabledType>::type;
+    /// @brief Enable if a type is a valid iterator that can NOT be used bi-directionally
+    template <typename _Iterator, typename _EnabledType = void>
+    using enable_if_iterator_not_reversible = typename std::enable_if<!std::is_base_of<std::bidirectional_iterator_tag, typename std::iterator_traits<_Iterator>::iterator_category>::value
+                                                                   && !is_bidirectional_iterator<_Iterator>::value
+                                                                   && !is_random_access_iterator<_Iterator>::value, _EnabledType>::type;
+
+    // -- constructors / assignment enablers --
+
+    template <bool _Copyable, bool _Movable> 
+    struct ConstructorEnabler {};
+    template <> struct ConstructorEnabler<false, false> {
+      ConstructorEnabler() = default;
+      ConstructorEnabler(const ConstructorEnabler& rhs) = delete;
+      ConstructorEnabler(ConstructorEnabler&& rhs) = delete;
+      ConstructorEnabler& operator=(const ConstructorEnabler& rhs) = default;
+      ConstructorEnabler& operator=(ConstructorEnabler&& rhs) = default;
+    };
+    template <> struct ConstructorEnabler<true, false> { 
+      ConstructorEnabler() = default;
+      ConstructorEnabler(const ConstructorEnabler& rhs) = default;
+      ConstructorEnabler(ConstructorEnabler&& rhs) = delete;
+      ConstructorEnabler& operator=(const ConstructorEnabler& rhs) = default;
+      ConstructorEnabler& operator=(ConstructorEnabler&& rhs) = default;
+    };
+    template <> struct ConstructorEnabler<false, true> {
+      ConstructorEnabler() = default;
+      ConstructorEnabler(const ConstructorEnabler& rhs) = delete;
+      ConstructorEnabler(ConstructorEnabler&& rhs) = default;
+      ConstructorEnabler& operator=(const ConstructorEnabler& rhs) = default;
+      ConstructorEnabler& operator=(ConstructorEnabler&& rhs) = default;
+    };
+    template <> struct ConstructorEnabler<true, true> {
+      ConstructorEnabler() = default;
+      ConstructorEnabler(const ConstructorEnabler& rhs) = default;
+      ConstructorEnabler(ConstructorEnabler&& rhs) = default;
+      ConstructorEnabler& operator=(const ConstructorEnabler& rhs) = default;
+      ConstructorEnabler& operator=(ConstructorEnabler&& rhs) = default;
+    };
+
+    template <typename T>
+    using ConstructorSelector = ConstructorEnabler<!std::is_class<T>::value || std::is_copy_constructible<T>::value, !std::is_class<T>::value || std::is_move_constructible<T>::value>;
+
+    template <bool _Copyable, bool _Movable>
+    struct AssignmentEnabler {};
+    template <> struct AssignmentEnabler<false, false> {
+      AssignmentEnabler() = default;
+      AssignmentEnabler(const AssignmentEnabler& rhs) = default;
+      AssignmentEnabler(AssignmentEnabler&& rhs) = default;
+      AssignmentEnabler& operator=(const AssignmentEnabler& rhs) = delete;
+      AssignmentEnabler& operator=(AssignmentEnabler&& rhs) = delete;
+    };
+    template <> struct AssignmentEnabler<true, false> {
+      AssignmentEnabler() = default;
+      AssignmentEnabler(const AssignmentEnabler& rhs) = default;
+      AssignmentEnabler(AssignmentEnabler&& rhs) = default;
+      AssignmentEnabler& operator=(const AssignmentEnabler& rhs) = default;
+      AssignmentEnabler& operator=(AssignmentEnabler&& rhs) = delete;
+    };
+    template <> struct AssignmentEnabler<false, true> {
+      AssignmentEnabler() = default;
+      AssignmentEnabler(const AssignmentEnabler& rhs) = default;
+      AssignmentEnabler(AssignmentEnabler&& rhs) = default;
+      AssignmentEnabler& operator=(const AssignmentEnabler& rhs) = delete;
+      AssignmentEnabler& operator=(AssignmentEnabler&& rhs) = default;
+    };
+    template <> struct AssignmentEnabler<true, true> {
+      AssignmentEnabler() = default;
+      AssignmentEnabler(const AssignmentEnabler& rhs) = default;
+      AssignmentEnabler(AssignmentEnabler&& rhs) = default;
+      AssignmentEnabler& operator=(const AssignmentEnabler& rhs) = default;
+      AssignmentEnabler& operator=(AssignmentEnabler&& rhs) = default;
+    };
+
+    template <typename T>
+    using AssignmentSelector = AssignmentEnabler<!std::is_class<T>::value || std::is_copy_assignable<T>::value, !std::is_class<T>::value || std::is_move_assignable<T>::value>;
+  }
+}
---
+++ pattern/test/class_traits_test.cpp
@@ -0,0 +1,278 @@
+#include <gtest/gtest.h>
+#include <vector>
+#include <list>
+#include <pattern/class_traits.h>
+
+using namespace pandora::pattern;
+
+class ClassTraitsTest : public testing::Test {
+public:
+protected:
+  //static void SetUpTestCase() {}
+  //static void TearDownTestCase() {}
+
+  void SetUp() override {}
+  void TearDown() override {}
+};
+
+
+// -- mocks --
+
+template <typename _DataType>
+class TraitsTester final {
+public:
+  TraitsTester() = default;
+  template <typename T = _DataType>
+  TraitsTester(enable_if_copy_constructible<T, const T& > value) : _value(value) {}
+  template <typename T = _DataType>
+  TraitsTester(enable_if_move_constructible<T, T&& > value) : _value(std::move(value)) {}
+  ~TraitsTester() = default;
+
+  const _DataType& value() const noexcept { return _value; }
+
+  // - copy / move -
+
+  template <typename T = _DataType>
+  TraitsTester(enable_if_copy_constructible<T, const TraitsTester<T>& > rhs) : _value(rhs._value) {}
+  template <typename T = _DataType>
+  TraitsTester(enable_if_move_constructible<T, TraitsTester<T>&& > rhs) : _value(std::move(rhs._value)) {}
+
+  template <typename T = _DataType>
+  enable_if_copy_constructible<T, TraitsTester<_DataType>& > operator=(const TraitsTester<T>& rhs) { _value = rhs._value; return *this; }
+  template <typename T = _DataType>
+  enable_if_move_constructible<T, TraitsTester<_DataType>& > operator=(TraitsTester<T>&& rhs) { _value = std::move(rhs._value); return *this; }
+
+  // - comparisons -
+
+  template <typename T = _DataType>
+  inline bool operator==(enable_if_operator_equals<T, const TraitsTester<T>& > rhs) const noexcept { return (this->_value == rhs._value); }
+  template <typename T = _DataType>
+  inline bool operator!=(enable_if_operator_equals<T, const TraitsTester<T>& > rhs) const noexcept { return !(this->operator==(rhs)); }
+
+  template <typename T = _DataType>
+  inline bool operator<(enable_if_operator_less<T, const TraitsTester<T>& > rhs) const noexcept { return (this->_value < rhs._value); }
+  template <typename T = _DataType>
+  inline bool operator<=(enable_if_operator_less_eq<T, const TraitsTester<T>& > rhs) const noexcept { return (this->_value <= rhs._value); }
+  template <typename T = _DataType>
+  inline bool operator>(enable_if_operator_greater<T, const TraitsTester<T>& > rhs) const noexcept { return (this->_value > rhs._value); }
+  template <typename T = _DataType>
+  inline bool operator>=(enable_if_operator_greater_eq<T, const TraitsTester<T>& > rhs) const noexcept { return (this->_value >= rhs._value); }
+
+private:
+  _DataType _value;
+};
+
+#define __P_GENERATE_MOCK(name, copyable, movable) \
+        template <typename T> class name { \
+        public: \
+          name() = default; \
+          name(const T& value) : _value(value) {} \
+          name(const name &) = copyable ; \
+          name & operator=(const name &) = copyable ; \
+          name(name &&) = movable ; \
+          name & operator=(name &&) = movable ; \
+          bool operator==(const name& rhs) const noexcept { return _value == rhs._value; } \
+          bool operator!=(const name& rhs) const noexcept { return _value != rhs._value; } \
+          bool operator<(const name& rhs) const noexcept { return _value < rhs._value; } \
+          bool operator<=(const name& rhs) const noexcept { return _value <= rhs._value; } \
+          bool operator>(const name& rhs) const noexcept { return _value > rhs._value; } \
+          bool operator>=(const name& rhs) const noexcept { return _value >= rhs._value; } \
+          void value(const T& val) noexcept { _value = val; } \
+          const T& value() const noexcept { return _value; } \
+        private: \
+          T _value; }
+
+using BaseType = int;
+__P_GENERATE_MOCK(Copyable, default, delete);
+__P_GENERATE_MOCK(Movable, delete, default);
+__P_GENERATE_MOCK(CopyMovable, default, default);
+
+
+// -- copy / move / comparisons --
+
+TEST_F(ClassTraitsTest, baseTypeTraits) {
+  TraitsTester<BaseType> data(42);
+
+  TraitsTester<BaseType> copy(data);
+  EXPECT_EQ(data.value(), copy.value());
+
+  EXPECT_TRUE(data.value() == copy.value());
+  EXPECT_FALSE(data.value() != copy.value());
+  EXPECT_TRUE(data.value() <= copy.value());
+  EXPECT_FALSE(data.value() < copy.value());
+  EXPECT_TRUE(data.value() >= copy.value());
+  EXPECT_FALSE(data.value() > copy.value());
+
+  TraitsTester<BaseType> small(4);
+  EXPECT_FALSE(small.value() == data.value());
+  EXPECT_TRUE(small.value() != data.value());
+  EXPECT_TRUE(small.value() <= data.value());
+  EXPECT_TRUE(small.value() < data.value());
+  EXPECT_FALSE(small.value() >= data.value());
+  EXPECT_FALSE(small.value() > data.value());
+
+  data = small;
+  EXPECT_EQ(small.value(), data.value());
+  EXPECT_TRUE(small.value() == data.value());
+  EXPECT_FALSE(small.value() != data.value());
+}
+
+TEST_F(ClassTraitsTest, copyOnlyTraits) {
+  TraitsTester<Copyable<int> > data(Copyable<int>(42));
+
+  TraitsTester<Copyable<int> > copy(data);
+  EXPECT_EQ(data.value(), copy.value());
+
+  EXPECT_TRUE(data.value() == copy.value());
+  EXPECT_FALSE(data.value() != copy.value());
+  EXPECT_TRUE(data.value() <= copy.value());
+  EXPECT_FALSE(data.value() < copy.value());
+  EXPECT_TRUE(data.value() >= copy.value());
+  EXPECT_FALSE(data.value() > copy.value());
+
+  TraitsTester<Copyable<int> > small(Copyable<int>(4));
+  EXPECT_FALSE(small.value() == data.value());
+  EXPECT_TRUE(small.value() != data.value());
+  EXPECT_TRUE(small.value() <= data.value());
+  EXPECT_TRUE(small.value() < data.value());
+  EXPECT_FALSE(small.value() >= data.value());
+  EXPECT_FALSE(small.value() > data.value());
+
+  data = small;
+  EXPECT_EQ(small.value(), data.value());
+  EXPECT_TRUE(small.value() == data.value());
+  EXPECT_FALSE(small.value() != data.value());
+}
+
+TEST_F(ClassTraitsTest, moveOnlyTraits) {
+  TraitsTester<Movable<int> > data(Movable<int>(42));
+  EXPECT_EQ(data.value(), 42);
+
+  TraitsTester<Movable<int> > moved(std::move(data));
+  EXPECT_EQ(moved.value(), 42);
+
+  data = TraitsTester<Movable<int> >(Movable<int>(4));
+  EXPECT_EQ(data.value(), 4);
+  data = TraitsTester<Movable<int> >(Movable<int>(42));
+  EXPECT_EQ(data.value(), 42);
+  EXPECT_TRUE(data.value() == moved.value());
+  EXPECT_FALSE(data.value() != moved.value());
+  EXPECT_TRUE(data.value() <= moved.value());
+  EXPECT_FALSE(data.value() < moved.value());
+  EXPECT_TRUE(data.value() >= moved.value());
+  EXPECT_FALSE(data.value() > moved.value());
+
+  TraitsTester<Movable<int> > small(Movable<int>(4));
+  EXPECT_FALSE(small.value() == data.value());
+  EXPECT_TRUE(small.value() != data.value());
+  EXPECT_TRUE(small.value() <= data.value());
+  EXPECT_TRUE(small.value() < data.value());
+  EXPECT_FALSE(small.value() >= data.value());
+  EXPECT_FALSE(small.value() > data.value());
+
+  moved = std::move(data);
+  EXPECT_FALSE(small.value() == moved.value());
+  EXPECT_TRUE(small.value() != moved.value());
+  EXPECT_TRUE(small.value() <= moved.value());
+  EXPECT_TRUE(small.value() < moved.value());
+  EXPECT_FALSE(small.value() >= moved.value());
+  EXPECT_FALSE(small.value() > moved.value());
+}
+
+TEST_F(ClassTraitsTest, copyMoveTraits) {
+  TraitsTester<CopyMovable<int> > data(CopyMovable<int>(42));
+  TraitsTester<CopyMovable<int> > copy(data);
+  EXPECT_EQ(data.value(), copy.value());
+  TraitsTester<CopyMovable<int> > moved(std::move(data));
+  EXPECT_EQ(copy.value(), moved.value());
+
+  data = copy;
+  EXPECT_EQ(data.value(), copy.value());
+  EXPECT_TRUE(data.value() == moved.value());
+  EXPECT_FALSE(data.value() != moved.value());
+  EXPECT_TRUE(data.value() <= moved.value());
+  EXPECT_FALSE(data.value() < moved.value());
+  EXPECT_TRUE(data.value() >= moved.value());
+  EXPECT_FALSE(data.value() > moved.value());
+
+  TraitsTester<CopyMovable<int> > small(CopyMovable<int>(4));
+  EXPECT_FALSE(small.value() == data.value());
+  EXPECT_TRUE(small.value() != data.value());
+  EXPECT_TRUE(small.value() <= data.value());
+  EXPECT_TRUE(small.value() < data.value());
+  EXPECT_FALSE(small.value() >= data.value());
+  EXPECT_FALSE(small.value() > data.value());
+
+  moved = std::move(data);
+  EXPECT_FALSE(small.value() == moved.value());
+  EXPECT_TRUE(small.value() != moved.value());
+  EXPECT_TRUE(small.value() <= moved.value());
+  EXPECT_TRUE(small.value() < moved.value());
+  EXPECT_FALSE(small.value() >= moved.value());
+  EXPECT_FALSE(small.value() > moved.value());
+}
+
+
+// -- iterators --
+
+template <typename _It>
+int getValue(enable_if_iterator<_It, const _It&> it) noexcept { return *it; }
+template <typename _It>
+int getValueBiDirectional(enable_if_iterator_reversible<_It, const _It&> it) noexcept { return *it; }
+
+TEST_F(ClassTraitsTest, iteratorTraits) {
+  EXPECT_TRUE(is_forward_iterator<std::vector<char>::iterator>::value);
+  EXPECT_TRUE(is_bidirectional_iterator<std::vector<char>::iterator>::value);
+  EXPECT_TRUE(is_random_access_iterator<std::vector<char>::iterator>::value);
+
+  EXPECT_TRUE(is_forward_iterator<std::list<char>::iterator>::value);
+  EXPECT_TRUE(is_bidirectional_iterator<std::list<char>::iterator>::value);
+  EXPECT_FALSE(is_random_access_iterator<std::list<char>::iterator>::value);
+
+  std::vector<int> vec;
+  vec.push_back(42);
+  vec.push_back(24);
+  EXPECT_TRUE(getValue<std::vector<int>::iterator>(vec.begin()) == 42);
+  EXPECT_TRUE(getValue<std::vector<int>::iterator>(vec.begin() + 1) == 24);
+  EXPECT_TRUE(getValueBiDirectional<std::vector<int>::iterator>(vec.begin()) == 42);
+  EXPECT_TRUE(getValueBiDirectional<std::vector<int>::iterator>(vec.begin() + 1) == 24);
+}
+
+
+// -- constructors / assignment enablers --
+
+template <typename T>
+struct EnablerTester : public ConstructorSelector<T>, public AssignmentSelector<T> {
+  EnablerTester() = default;
+  EnablerTester(const EnablerTester<T>& rhs) : ConstructorSelector<T>(rhs) { value = rhs.value; }
+  EnablerTester(EnablerTester<T>&& rhs) noexcept : ConstructorSelector<T>(std::move(rhs)) { value = std::move(rhs.value); }
+  EnablerTester& operator=(const EnablerTester<T>& rhs) { AssignmentSelector<T>::operator=(rhs);  value = rhs.value; }
+  EnablerTester& operator=(EnablerTester<T>&& rhs) noexcept { AssignmentSelector<T>::operator=(std::move(rhs)); value = std::move(rhs.value); }
+  T value;
+};
+
+TEST_F(ClassTraitsTest, enablers) {
+  EnablerTester<BaseType> baseType;
+  baseType.value = 42;
+  EnablerTester<BaseType> baseCopy(baseType);
+  EnablerTester<BaseType> baseMove(std::move(baseType));
+  EXPECT_TRUE(baseCopy.value == 42);
+  EXPECT_TRUE(baseMove.value == 42);
+
+  EnablerTester<Copyable<int> > copyable;
+  copyable.value.value(42);
+  EnablerTester<Copyable<int> > copyableCopy(copyable);
+  EXPECT_TRUE(copyableCopy.value.value() == 42);
+
+  EnablerTester<Movable<int> > movable;
+  movable.value.value(42);
+  EnablerTester<Movable<int> > movableMove(std::move(movable));
+  EXPECT_TRUE(movableMove.value.value() == 42);
+
+  EnablerTester<CopyMovable<int> > copyMovable;
+  copyMovable.value.value(42);
+  EnablerTester<CopyMovable<int> > copyMovableCopy(copyMovable);
+  EnablerTester<CopyMovable<int> > copyMovableMove(std::move(copyMovable));
+  EXPECT_TRUE(copyableCopy.value.value() == 42);
+  EXPECT_TRUE(copyMovableMove.value.value() == 42);
+}
