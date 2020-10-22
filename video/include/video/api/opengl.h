550df2bb99fd813fb9584239dbd6b93de89fd0e7
vinders <vinders@users.noreply.github.com>
2020-10-22 12:11:17 +0200

    feat-video: openGL/vulkan API includes

---
+++ video/include/video/api/opengl.h
@@ -0,0 +1,27 @@
+/*******************************************************************************
+Author  :     Romain Vinders
+License :     MIT
+*******************************************************************************/
+#pragma once
+
+#include <cstdint>
+#include <cstddef>
+#include <stdexcept>
+
+#if !defined(_WINDOWS) && defined(__APPLE__)
+# include <TargetConditionals.h>
+# if defined(TARGET_OS_IPHONE) && TARGET_OS_IPHONE
+#   include <OpenGLES/ES2/gl.h>
+#   include <OpenGLES/ES2/glext.h>
+# else
+#   include <OpenGL/gl.h>
+# endif
+#elif defined(__ANDROID__)
+# include <GLES/gl.h>
+# include <GLES/glext.h>
+#else
+# ifndef GLEW_STATIC
+#  define GLEW_STATIC
+# endif
+# include <GL/glew.h>
+#endif
---
+++ video/include/video/api/vulkan.h
@@ -0,0 +1,12 @@
+/*******************************************************************************
+Author  :     Romain Vinders
+License :     MIT
+*******************************************************************************/
+#pragma once
+
+#include <cstdint>
+
+//#define VK_NO_PROTOTYPES 1 // only when dynamic linking
+#include <vulkan/vulkan.h>
+
+#define GLFW_INCLUDE_VULKAN
