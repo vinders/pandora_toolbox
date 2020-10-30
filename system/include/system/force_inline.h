f7021ccd19dfe00096d3bc594915c654b1564c37
vinders <vinders@users.noreply.github.com>
2020-10-30 22:45:24 +0100

    patch-system: portable 'force inline' keyword: compiler compatibility

--- system/include/system/force_inline.h
+++ system/include/system/force_inline.h
@@ -7,18 +7,12 @@ Constants : __forceinline
 #pragma once
 
 #ifndef __forceinline
-# if defined(__clang__)
-#   define __forceinline __attribute__((always_inline))
-
-# elif defined(__GNUC__) || defined(__GNUG__)
-#   define __forceinline __attribute__((always_inline))
-
-# elif defined(_MSC_VER)
+# if defined(_MSC_VER)
 #   if _MSC_VER < 1900
 #     define __forceinline inline
 #   endif
 
-# elif defined(__MINGW32__)
+# elif defined(__clang__) || defined(__GNUC__) || defined(__GNUG__) || defined(__MINGW32__)
 #   define __forceinline __attribute__((always_inline))
 
 # else
