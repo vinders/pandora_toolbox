5fe333c77bc550c174ddcfd5ed34a04c95773edc
vinders <vinders@users.noreply.github.com>
2020-11-19 20:15:43 +0100

    patch-system: win32 API version: rename cmake constant

--- system/include/system/api/windows_version.h
+++ system/include/system/api/windows_version.h
@@ -22,33 +22,33 @@ Description : Microsoft Windows feature version constants
 #   undef NTDDI_VERSION
 # endif
 
-# if defined(_MIN_WINDOWS_VERSION) && _MIN_WINDOWS_VERSION == 10 // only Windows 10+
-#   define WINVER _WIN32_WINNT_WIN10 
-#   define _WIN32_WINNT _WIN32_WINNT_WIN10 
-#   define _WIN32_WINDOWS _WIN32_WINNT_WIN10   
-#   define _WIN32_IE _WIN32_IE_IE110  
-#   define NTDDI_VERSION NTDDI_WIN10_RS3 
+# if defined(_P_MIN_WINDOWS_VERSION) && _P_MIN_WINDOWS_VERSION == 10 // only Windows 10 RS3+
+#   define WINVER _WIN32_WINNT_WIN10
+#   define _WIN32_WINNT _WIN32_WINNT_WIN10
+#   define _WIN32_WINDOWS _WIN32_WINNT_WIN10
+#   define _WIN32_IE _WIN32_IE_IE110
+#   define NTDDI_VERSION NTDDI_WIN10_RS3
 
-# elif defined(_MIN_WINDOWS_VERSION) && _MIN_WINDOWS_VERSION == 8 // Windows 8/10+
-#   define WINVER _WIN32_WINNT_WIN8 
-#   define _WIN32_WINNT _WIN32_WINNT_WIN8 
-#   define _WIN32_WINDOWS _WIN32_WINNT_WIN8   
-#   define _WIN32_IE _WIN32_IE_IE90  
-#   define NTDDI_VERSION NTDDI_WIN8 
+# elif defined(_P_MIN_WINDOWS_VERSION) && _P_MIN_WINDOWS_VERSION == 8 // Windows 8/10+
+#   define WINVER _WIN32_WINNT_WIN8
+#   define _WIN32_WINNT _WIN32_WINNT_WIN8
+#   define _WIN32_WINDOWS _WIN32_WINNT_WIN8
+#   define _WIN32_IE _WIN32_IE_IE90
+#   define NTDDI_VERSION NTDDI_WIN8
 
-# elif !defined(_MIN_WINDOWS_VERSION) || _MIN_WINDOWS_VERSION == 7 // Windows 7/8/10+
-#   define WINVER _WIN32_WINNT_WIN7 
-#   define _WIN32_WINNT _WIN32_WINNT_WIN7 
-#   define _WIN32_WINDOWS _WIN32_WINNT_WIN7   
-#   define _WIN32_IE _WIN32_IE_IE80  
-#   define NTDDI_VERSION NTDDI_WIN7 
+# elif !defined(_P_MIN_WINDOWS_VERSION) || _P_MIN_WINDOWS_VERSION == 7 // Windows 7/8/10+
+#   define WINVER _WIN32_WINNT_WIN7
+#   define _WIN32_WINNT _WIN32_WINNT_WIN7
+#   define _WIN32_WINDOWS _WIN32_WINNT_WIN7
+#   define _WIN32_IE _WIN32_IE_IE80
+#   define NTDDI_VERSION NTDDI_WIN7
 
 # else // Windows Vista/7/8/10+
-#   define WINVER _WIN32_WINNT_VISTA 
-#   define _WIN32_WINNT _WIN32_WINNT_VISTA 
-#   define _WIN32_WINDOWS _WIN32_WINNT_VISTA   
-#   define _WIN32_IE _WIN32_IE_IE70  
-#   define NTDDI_VERSION NTDDI_VISTASP1 
+#   define WINVER _WIN32_WINNT_VISTA
+#   define _WIN32_WINNT _WIN32_WINNT_VISTA
+#   define _WIN32_WINDOWS _WIN32_WINNT_VISTA
+#   define _WIN32_IE _WIN32_IE_IE70
+#   define NTDDI_VERSION NTDDI_VISTASP1
 # endif
 
 #endif
