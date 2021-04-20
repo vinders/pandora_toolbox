c216a4dd3eec29511e672460f76dd3ccc3306459
vinders <vinders@users.noreply.github.com>
2021-04-20 18:19:09 +0200

    infra-cwork: custom cmake framework: add module for Android "glue" (executables startup)

---
+++ _cmake/modules/Findandroid_glue.cmake
@@ -0,0 +1,12 @@
+set(android_glue__FOUND ON)
+
+# Android
+if(ANDROID)
+    set(android_glue__INCLUDE 
+        ${ANDROID_NDK}/sources/android/native_app_glue 
+        ${ANDROID_NDK}/sources
+    )
+    set(android_glue__SOURCE 
+        ${ANDROID_NDK}/sources/android/native_app_glue/android_native_app_glue.c
+    )
+endif()
