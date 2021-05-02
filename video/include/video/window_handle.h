cad27b329bca25a519bf0c80a560ec45ee647115
vinders <vinders@users.noreply.github.com>
2021-05-02 13:18:59 +0200

    patch-video: generic window and resource handles

--- video/include/video/window_handle.h
+++ video/include/video/window_handle.h
@@ -4,14 +4,20 @@ License :     MIT
 *******************************************************************************/
 #pragma once
 
-#include <cstdint>
+#if !defined(_WINDOWS) && !defined(__APPLE__)
+# include <cstdint>
+#endif
 
 namespace pandora {
   namespace video {
-#     if defined(_WINDOWS) || defined(__APPLE__)
-        using WindowHandle = void*;
-#     else
-        using WindowHandle = uint32_t;
-#     endif
+#   if defined(_WINDOWS) || defined(__APPLE__)
+      using WindowHandle = void*;         ///< Handle to native system window
+      using WindowResourceHandle = void*; ///< Handle to native window resource (icon, cursor, color brush, ...)
+      using MenuHandle = void*;           ///< Handle to native menu instance
+#   else
+      using WindowHandle = uint32_t;      ///< Handle to native system window
+      using WindowResourceHandle = void*; ///< Handle to native window resource (icon, cursor, color brush, ...)
+      using MenuHandle = uint32_t;        ///< Handle to native menu instance
+#   endif
   }
 }
