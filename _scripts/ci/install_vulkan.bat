bc31d9eef97bdd4b6c7554dfd30f1988066df60d
vinders <vinders@users.noreply.github.com>
2021-09-16 14:24:04 +0200

    infra-ci: appveyor continuous integration: add vulkan

--- _scripts/ci/install_vulkan.bat
+++ _scripts/ci/install_vulkan.bat
@@ -3,33 +3,29 @@
 set _VULKAN_SDK_VERSION=1.2.189.2
 
 :: fetch vulkan SDK installer
-set /A retry = 1
-:loop
 echo "Fetching vulkan SDK installer..."
-curl -L --silent --show-error --output VulkanSDK.exe "https://vulkan.lunarg.com/sdk/download/%_VULKAN_SDK_VERSION%/windows/VulkanSDK-%_VULKAN_SDK_VERSION%-Installer.exe?Human=true"
-
-if %retry%==5 goto end 
-set /a retry=%retry%+1 
-if not exist ./VulkanSDK.exe goto loop
-:end
-
 if not exist ./VulkanSDK.exe (
-    echo "Failed to obtain installer..."
-    exit /B 1
+    curl -L --silent --show-error --output VulkanSDK.exe "https://vulkan.lunarg.com/sdk/download/%_VULKAN_SDK_VERSION%/windows/VulkanSDK-%_VULKAN_SDK_VERSION%-Installer.exe?Human=true"
+    if not exist ./VulkanSDK.exe (
+        echo "Failed to obtain installer..."
+        exit /B 1
+    )
 )
 
 :: run installer
+echo "Starting install..."
 start /wait "VulkanSDK.exe /S"
-del /f VulkanSDK.exe
-echo "Vulkan SDK install complete!"
+if not exist C:\VulkanSDK\%_VULKAN_SDK_VERSION%\ (
+    echo "Failed to install..."
+    exit /B 1
+)
 
-if exist C:\VulkanSDK\%_VULKAN_SDK_VERSION%\ (
-    if "%VULKAN_SDK%"=="" (
-        setx VULKAN_SDK C:\VulkanSDK\%_VULKAN_SDK_VERSION%
-        setx VK_SDK_PATH C:\VulkanSDK\%_VULKAN_SDK_VERSION%
-        set VULKAN_SDK=C:\VulkanSDK\%_VULKAN_SDK_VERSION%
-        set VK_SDK_PATH=C:\VulkanSDK\%_VULKAN_SDK_VERSION%
-    )
+echo "Vulkan SDK install complete!"
+if "%VULKAN_SDK%"=="" (
+    setx VULKAN_SDK C:\VulkanSDK\%_VULKAN_SDK_VERSION%
+    setx VK_SDK_PATH C:\VulkanSDK\%_VULKAN_SDK_VERSION%
+    set VULKAN_SDK=C:\VulkanSDK\%_VULKAN_SDK_VERSION%
+    set VK_SDK_PATH=C:\VulkanSDK\%_VULKAN_SDK_VERSION%
 )
   
 exit /B 0
