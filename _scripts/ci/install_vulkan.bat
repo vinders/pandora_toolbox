ef0a63ee06c0c5618ef73ffe9e5351824a611d07
vinders <vinders@users.noreply.github.com>
2021-09-16 14:59:57 +0200

    infra-ci: appveyor continuous integration: add vulkan

--- _scripts/ci/install_vulkan.bat
+++ _scripts/ci/install_vulkan.bat
@@ -3,8 +3,8 @@
 set _VULKAN_SDK_VERSION=1.2.189.0
 
 :: fetch vulkan SDK installer
-echo "Fetching vulkan SDK installer..."
 if not exist ./VulkanSDK-1.2.exe (
+    echo "Fetching vulkan SDK installer..."
     curl -L --silent --show-error --output VulkanSDK-1.2.exe "https://vulkan.lunarg.com/sdk/download/%_VULKAN_SDK_VERSION%/windows/VulkanSDK-%_VULKAN_SDK_VERSION%-Installer.exe?Human=true"
     if not exist ./VulkanSDK-1.2.exe (
         echo "Failed to obtain installer..."
@@ -13,11 +13,21 @@ if not exist ./VulkanSDK-1.2.exe (
 )
 
 :: run installer
-echo "Starting install..."
+echo "Disable UAC popups..."
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 00000000 /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v ConsentPromptBehaviorUser /t REG_DWORD /d 00000000 /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_DWORD /d 00000000 /f
-start /wait "VulkanSDK-1.2.exe /S"
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v PromptOnSecureDesktop /t REG_DWORD /d 00000000 /f
+
+echo "Starting install..."
+start /MIN cmd /C "TIMEOUT /T 300 /NOBREAK & TASKKILL VulkanSDK-1.2.exe" & start /wait "VulkanSDK-1.2.exe /S"
 if not exist C:\VulkanSDK\%_VULKAN_SDK_VERSION%\ (
+    dir /b C:\VulkanSDK\
+    echo "Failed to install..."
+    exit /B 1
+)
+if not exist C:\VulkanSDK\%_VULKAN_SDK_VERSION%\Include\ (
+    dir /b C:\VulkanSDK\%_VULKAN_SDK_VERSION%\
     echo "Failed to install..."
     exit /B 1
 )
