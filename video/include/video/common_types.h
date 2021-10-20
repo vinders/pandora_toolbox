3627065844300567d941a8bf717eeee46d791b02
vinders <vinders@users.noreply.github.com>
2021-10-20 20:36:02 +0200

    patch-video: swap-chain descriptor: add present mode

--- video/include/video/common_types.h
+++ video/include/video/common_types.h
@@ -26,20 +26,32 @@ namespace pandora {
     // -- resource creation param types --
 
     /// @brief Advanced swap-chain target output params (bit-mask flags)
-    /// @warning - Some of these features are not supported by the OpenGL API (ignored if not supported).
-    ///          - Some of these features are not supported by old API versions (Direct3D 11.0, OpenGL 4.1...)
+    /// @warning Some of these features are not supported by some APIs (OpenGL) or by old API versions (Direct3D 11.0, OpenGL 4.1...).
+    ///          They'll be ignored if they're not supported.
     enum class TargetOutputFlag : uint32_t {
       none            = 0x0u, ///< No option
-      variableRefresh = 0x1u, ///< Variable refresh rate display in fullscreen/borderless.
-      partialOutput   = 0x2u, ///< Partial display output config (instead of unique display output):
-                              ///  allows multiple partial swap-chains (for example, for multi-window rendering).
-      stereo          = 0x4u, ///< Stereo rendering mode, for 3D devices/glasses (only usable in fullscreen mode).
+      variableRefresh = 0x1u, ///< Variable refresh rate display in fullscreen/borderless (tearing).
+      stereo          = 0x2u, ///< Stereo rendering mode, for 3D devices/glasses (only usable in fullscreen mode).
       shaderInput     = 0x10u,///< Allow using swap-chain output as shader input data.
-      disableFlipSwap = 0x20u,///< Prevent swap-chain from using flip-swap mode if supported.
+      disableFlipSwap = 0x20u,///< Prevent swap-chain from using flip-swap/mailbox mode if supported:
+                              ///  - forces vsync to always block calling thread during buffer swap, even if framebufferCount > 2.
+                              ///  - prevents window from being associated with DWM on Windows.
+                              ///  - allows partial output swap-chains to be created with Direct3D -> multiple swap-chains allowed for the same window.
+                              ///  - warning: a less efficient swapping method will be used instead.
       swapNoDiscard   = 0x40u ///< Keep existing data after swapping buffers if supported:
                               ///  - GPU resources not released as efficiently -> should only be used if frames aren't completely redrawn.
                               ///  - Required when previous data must be kept (otherwise, the buffer would contain garbage after being swapped).
                               ///  - Can also be useful if 'clearView' is called after every swap call (clearing the buffer also releases GPU resources).
+    };
+
+    /// @brief Swap-chain presentation mode (when swapping buffers)
+    enum class PresentMode : uint32_t {
+      immediate = 0u, ///< Immediate display (no wait for vertical blanking): most reactive, may cause screen tearing
+                      ///  (supported by most GPUs; will use fifo mode instead if not).
+      fifo      = 1u, ///< Vsync / FIFO mode (wait for vertical blanking): no tearing, may cause latency (supported by all GPUs).
+                      ///  Note: - allows triple buffering when flip-swap/mailbox is enabled and 3 buffers are created ('framebufferCount').
+                      ///        - flip-swap/mailbox is enabled by default if supported (D3D 11.2+ / Vulkan / OpenGL if enabled in driver)
+                      ///          and if neither TargetOutputFlag::partialOutput nor TargetOutputFlag::disableFlipSwap are set.
     };
     
     // ---
@@ -85,9 +97,10 @@ namespace pandora {
     /// @remarks The same params can be used to build multiple SwapChain instances.
     struct SwapChainDescriptor final {
       SwapChainDescriptor() noexcept = default;
-      SwapChainDescriptor(uint32_t width, uint32_t height, uint32_t framebufferCount = 2u, 
+      SwapChainDescriptor(uint32_t width, uint32_t height, uint32_t framebufferCount = 2u, PresentMode presentMode = PresentMode::fifo,
                           RefreshRate refreshRate = RefreshRate{}, TargetOutputFlag outputFlags = TargetOutputFlag::none) noexcept
-        : width(width), height(height), framebufferCount(framebufferCount), refreshRate(refreshRate), outputFlags(outputFlags) {}
+        : width(width), height(height), framebufferCount(framebufferCount), 
+          presentMode(presentMode), refreshRate(refreshRate), outputFlags(outputFlags) {}
 
       SwapChainDescriptor(const SwapChainDescriptor&) = default;
       SwapChainDescriptor& operator=(const SwapChainDescriptor&) = default;
@@ -96,7 +109,8 @@ namespace pandora {
       uint32_t width = 0;   ///< Framebuffer pixel width (usually the window client width, except in special cases (emulator with internal resolution...))
       uint32_t height = 0;  ///< Framebuffer pixel height (usually the window client height, except in special cases (emulator with internal resolution...))
       uint32_t framebufferCount = 2u; ///< number of framebuffers (back-buffers + front-buffer) -- default: 2 (minimum: 1).
-      RefreshRate refreshRate;        ///< framebuffer refresh rate -- default: 60Hz.
+      PresentMode presentMode = PresentMode::fifo; ///< vsync/mailbox (fifo) or immediate display.
+      RefreshRate refreshRate;                     ///< framebuffer refresh rate -- default: 60Hz.
       TargetOutputFlag outputFlags = TargetOutputFlag::none; ///< special swap-chain output settings.
     };
   }
