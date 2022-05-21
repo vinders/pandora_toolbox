05081a74d6f79f98b21078d6306f3fadf9233076
vinders <vinders@users.noreply.github.com>
2022-05-21 14:13:46 +0200

    infra-scripts: version+changelog generation scripts: refactor + support "break-feat" and "break-fix" prefixes + generate cmake version file

--- _scripts/update_changelog.sh
+++ _scripts/update_changelog.sh
@@ -1,74 +1,93 @@
 #!/bin/bash
 
-# Identify version file
-FILENAME="./CHANGELOG.md"
+VERSION_FILENAME="./Version.cmake"
+CHANGELOG_FILENAME="./CHANGELOG.md"
+
+# Identify project directory
 if ! [ -e "./CMakeLists.txt" ]; then
-    cd ..
-    if ! [ -e "./CMakeLists.txt" ]; then
-        echo "project directory not found in current directory nor in parent directory!"
-        exit 1
-    fi
+  cd ..
+  if ! [ -e "./CMakeLists.txt" ]; then
+    echo "project directory not found in current directory nor in parent directory!"
+    exit 1
+  fi
 fi
 
-# update/get project version + revision
-sh ./_scripts/update_version.sh
-read -r CURRENT_VERSION<build_version.txt
+# Get last and current commit revisions (last revision read from changelog)
 CURRENT_REVISION=$(git rev-parse HEAD)
-# get last logged revision
-LAST_REVISION=$(cat $FILENAME | head -5 | tail -1 | awk '{ print $NF }')
-if [ -n "$LAST_REVISION" ]; then
-  LAST_REVISION_TYPE=$(git cat-file -t ${LAST_REVISION})
+if [ -e "$CHANGELOG_FILENAME" ]; then
+  LAST_REVISION=$(cat "$CHANGELOG_FILENAME" | head -5 | tail -1 | awk '{ print $NF }')
+  if [ -n "$LAST_REVISION" ]; then
+    LAST_REVISION_TYPE=$(git cat-file -t ${LAST_REVISION})
+  fi
 fi
 
-# count changes
+# Update and read project version
+sh ./_scripts/update_version.sh
+if ! [ -e "$VERSION_FILENAME" ]; then
+  echo "failed to generate version file..."
+  exit 1
+fi
+CURRENT_VERSION=$(cat "$VERSION_FILENAME" | head -1 | tail -1 | awk '{ print $NF }')
+
+# Count all changes since last revision
 if [ -n "$LAST_REVISION_TYPE" ] && [ "$LAST_REVISION_TYPE" = "commit" ]; then
-  NB_MAJOR=$(git rev-list -n 999999999 --grep='^major-' -i --count ${LAST_REVISION}..HEAD)
+  NB_BREAK_FEATURES=$(git rev-list -n 999999999 --grep='^break-feat-' -i --count ${LAST_REVISION}..HEAD)
+  NB_BREAK_FIXES=$(git rev-list -n 999999999 --grep='^break-fix-' -i --count ${LAST_REVISION}..HEAD)
   NB_FEATURES=$(git rev-list -n 999999999 --grep='^feat-' -i --count ${LAST_REVISION}..HEAD)
   NB_FIXES=$(git rev-list -n 999999999 --grep='^fix-' -i --count ${LAST_REVISION}..HEAD)
 else
-  NB_MAJOR=$(git rev-list -n 999999999 --grep='^major-' -i --count HEAD)
+  NB_BREAK_FEATURES=$(git rev-list -n 999999999 --grep='^break-feat-' -i --count HEAD)
+  NB_BREAK_FIXES=$(git rev-list -n 999999999 --grep='^break-fix-' -i --count HEAD)
   NB_FEATURES=$(git rev-list -n 999999999 --grep='^feat-' -i --count HEAD)
   NB_FIXES=$(git rev-list -n 999999999 --grep='^fix-' -i --count HEAD)
 fi
 
 # Update changelog file
-if [ $NB_MAJOR -gt 0 ] || [ $NB_FEATURES -gt 0 ] || [ $NB_FIXES -gt 0 ]; then
+if [ $NB_BREAK_FEATURES -gt 0 ] || [ $NB_BREAK_FIXES -gt 0 ] || [ $NB_FEATURES -gt 0 ] || [ $NB_FIXES -gt 0 ]; then
   CHANGES="# Changelog\nAll notable changes will be documented in this file (generated).\n\n## ${CURRENT_VERSION}\n$(date +'%Y-%m-%d') - $CURRENT_REVISION"
-  if [ -n "$LAST_REVISION_TYPE" ] && [ "$LAST_REVISION_TYPE" = "commit" ]; then
-    if [ $NB_MAJOR -gt 0 ]; then
-      CHANGES+="\n### major changes\n"
-      MAJOR_DATA=$(git shortlog --grep='^major-' -i ${LAST_REVISION}..HEAD)
-      CHANGES+="${MAJOR_DATA//      major-/* }"
+  
+  if [ $NB_BREAK_FEATURES -gt 0 ]; then
+    CHANGES+="\n### features (major)\n"
+    if [ -n "$LAST_REVISION_TYPE" ] && [ "$LAST_REVISION_TYPE" = "commit" ]; then
+      BREAK_FEAT_DATA=$(git shortlog --grep='^break-feat-' -i ${LAST_REVISION}..HEAD)
+    else
+      BREAK_FEAT_DATA=$(git shortlog --grep='^break-feat-' -i)
     fi
-    if [ $NB_FEATURES -gt 0 ]; then
-      CHANGES+="\n### features\n"
-      FEAT_DATA=$(git shortlog --grep='^feat-' -i ${LAST_REVISION}..HEAD)
-      CHANGES+="${FEAT_DATA//      feat-/* }"
-    fi
-    if [ $NB_FIXES -gt 0 ]; then
-      CHANGES+="\n### fixes\n"
-      FIX_DATA=$(git shortlog --grep='^fix-' -i ${LAST_REVISION}..HEAD)
-      CHANGES+="${FIX_DATA//      fix-/* }"
-    fi
-  else
-    if [ $NB_MAJOR -gt 0 ]; then
-      CHANGES+="\n### major changes\n"
-      MAJOR_DATA=$(git shortlog --grep='^major-' -i)
-      CHANGES+="${MAJOR_DATA//      major-/* }"
-    fi
-    if [ $NB_FEATURES -gt 0 ]; then
-      CHANGES+="\n### features\n"
-      FEAT_DATA=$(git shortlog --grep='^feat-' -i)
-      CHANGES+="${FEAT_DATA//      feat-/* }"
-    fi
-    if [ $NB_FIXES -gt 0 ]; then
-      CHANGES+="\n### fixes\n"
-      FIX_DATA=$(git shortlog --grep='^fix-' -i)
-      CHANGES+="${FIX_DATA//      fix-/* }"
-    fi
+    CHANGES+="${BREAK_FEAT_DATA//      break-feat-/* }"
   fi
-
-  printf "${CHANGES}\n\n---\n$(tail -n +3 ${FILENAME})" > $FILENAME
+  if [ $NB_FEATURES -gt 0 ]; then
+    CHANGES+="\n### features\n"
+    if [ -n "$LAST_REVISION_TYPE" ] && [ "$LAST_REVISION_TYPE" = "commit" ]; then
+      FEAT_DATA=$(git shortlog --grep='^feat-' -i ${LAST_REVISION}..HEAD)
+    else
+      FEAT_DATA=$(git shortlog --grep='^feat-' -i)
+    fi
+    CHANGES+="${FEAT_DATA//      feat-/* }"
+  fi
+  if [ $NB_BREAK_FIXES -gt 0 ]; then
+    CHANGES+="\n### fixes (major)\n"
+    if [ -n "$LAST_REVISION_TYPE" ] && [ "$LAST_REVISION_TYPE" = "commit" ]; then
+      BREAK_FIX_DATA=$(git shortlog --grep='^break-fix-' -i ${LAST_REVISION}..HEAD)
+    else
+      BREAK_FIX_DATA=$(git shortlog --grep='^break-fix-' -i)
+    fi
+    CHANGES+="${BREAK_FIX_DATA//      break-fix-/* }"
+  fi
+  if [ $NB_FIXES -gt 0 ]; then
+    CHANGES+="\n### fixes\n"
+    if [ -n "$LAST_REVISION_TYPE" ] && [ "$LAST_REVISION_TYPE" = "commit" ]; then
+      FIX_DATA=$(git shortlog --grep='^fix-' -i ${LAST_REVISION}..HEAD)
+    else
+      FIX_DATA=$(git shortlog --grep='^fix-' -i)
+    fi
+    CHANGES+="${FIX_DATA//      fix-/* }"
+  fi
+  
+  if [ -e "$CHANGELOG_FILENAME" ]; then
+    printf "${CHANGES}\n\n---\n$(tail -n +3 ${CHANGELOG_FILENAME})" > "${CHANGELOG_FILENAME}"
+  else
+    printf "${CHANGES}\n\n---\n" > "${CHANGELOG_FILENAME}"
+  fi
 fi
 
 exit 0
--- _scripts/update_version.sh
+++ _scripts/update_version.sh
@@ -1,28 +1,71 @@
 #!/bin/bash
 
-# Identify version file
-FILENAME="./build_version.txt"
+VERSION_FILENAME="./Version.cmake"
+CHANGELOG_FILENAME="./CHANGELOG.md"
+
+# Identify project directory
 if ! [ -e "./CMakeLists.txt" ]; then
-    cd ..
-    if ! [ -e "./CMakeLists.txt" ]; then
-        echo "project directory not found in current directory nor in parent directory!"
-        exit 1
+  cd ..
+  if ! [ -e "./CMakeLists.txt" ]; then
+    echo "project directory not found in current directory nor in parent directory!"
+    exit 1
+  fi
+fi
+
+# Get last commit revision + last project version (read from changelog)
+if [ -e "$CHANGELOG_FILENAME" ]; then
+  LAST_VERSION=$(cat "$CHANGELOG_FILENAME" | head -4 | tail -1 | awk '{ print $NF }')
+  if [ -n "$LAST_VERSION" ]; then
+    IFS='.'
+    read -ra LAST_VERSION_ARRAY <<< "$LAST_VERSION"
+    LAST_VERSION_MAJOR="${LAST_VERSION_ARRAY[0]}"
+    LAST_VERSION_MINOR="${LAST_VERSION_ARRAY[1]}"
+    LAST_VERSION_PATCH="${LAST_VERSION_ARRAY[2]}"
+    LAST_VERSION_TOTAL="${LAST_VERSION_ARRAY[3]}"
+    
+    LAST_REVISION=$(cat "$CHANGELOG_FILENAME" | head -5 | tail -1 | awk '{ print $NF }')
+    if [ -z "$LAST_REVISION" ] || [ -z "$LAST_VERSION_TOTAL" ]; then
+      unset LAST_VERSION_MAJOR
+      unset LAST_VERSION_MINOR
+      unset LAST_VERSION_PATCH
+      unset LAST_VERSION_TOTAL
+      unset LAST_REVISION
+    else
+      echo "> old version: ${LAST_VERSION_MAJOR}.${LAST_VERSION_MINOR}.${LAST_VERSION_PATCH}.${LAST_VERSION_TOTAL}"
     fi
+  fi
 fi
 
-# Get major version - count breaking changes
-MAJOR=$(git rev-list -n 999999999 --grep='^major-' -i --count HEAD)
-if ! [ "$MAJOR" = "0" ]; then
-  MAJOR_REV=$(git rev-list -n 999999999 --grep='^major-' -i -1 HEAD)
-fi
 
-# Get minor version - count feature changes since last break
-if [ -z "$MAJOR_REV" ]; then
-  MINOR=$(git rev-list -n 999999999 --grep='^feat-' -i --count HEAD)
+# -- READ VERSION BASED ON COMMIT PREFIXES -- ----------------------------------
+
+# Find major commits - count breaking changes ("break-...")
+if [ -z "$LAST_REVISION" ]; then
+  MAJOR_NB=$(git rev-list -n 999999999 --grep='^break-' -i --count HEAD)
 else
-  MINOR=$(git rev-list -n 999999999 --grep='^feat-' -i --count ${MAJOR_REV}..HEAD)
+  MAJOR_NB=$(git rev-list -n 999999999 --grep='^break-' -i --count ${LAST_REVISION}..HEAD)
 fi
-if ! [ "$MINOR" = "0" ]; then
+if ! [ "$MAJOR_NB" = "0" ]; then
+  MAJOR_REV=$(git rev-list -n 999999999 --grep='^break-' -i -1 HEAD)
+fi
+# Compute major version
+if [ -z "$LAST_VERSION_MAJOR" ]; then
+  MAJOR_VERSION=$(expr $MAJOR_NB)
+else
+  MAJOR_VERSION=$(expr $LAST_VERSION_MAJOR + $MAJOR_NB)
+fi
+
+# Find minor commits - count features since MAJOR_REV ("feat-...")
+if [ -z "$MAJOR_REV" ]; then
+  if [ -z "$LAST_REVISION" ]; then
+    MINOR_NB=$(git rev-list -n 999999999 --grep='^feat-' -i --count HEAD)
+  else
+    MINOR_NB=$(git rev-list -n 999999999 --grep='^feat-' -i --count ${LAST_REVISION}..HEAD)
+  fi
+else
+  MINOR_NB=$(git rev-list -n 999999999 --grep='^feat-' -i --count ${MAJOR_REV}..HEAD)
+fi
+if ! [ "$MINOR_NB" = "0" ]; then
   MINOR_REV=$(git rev-list -n 999999999 --grep='^feat-' -i -1 HEAD)
 fi
 if [ -z "$MINOR_REV" ]; then
@@ -30,20 +73,51 @@ if [ -z "$MINOR_REV" ]; then
     MINOR_REV=MAJOR_REV
   fi
 fi
-
-# Get patch version - count fixes since last feature
-if [ -z "$MINOR_REV" ]; then
-  PATCH=$(git rev-list -n 999999999 --grep='^fix-' -i --count HEAD)
+# Compute minor version
+if [ -z "$LAST_VERSION_MINOR" ] || ! [ "$MAJOR_NB" = "0" ]; then
+  MINOR_VERSION=$MINOR_NB
 else
-  PATCH=$(git rev-list -n 999999999 --grep='^fix-' -i --count ${MINOR_REV}..HEAD)
+  MINOR_VERSION=$(expr $LAST_VERSION_MINOR + $MINOR_NB)
 fi
 
-# Total number of commits
-TOTAL=$(git rev-list -n 999999999 --count HEAD)
+# Find patch commits - count fixes since last feature ("patch-...")
+if [ -z "$MINOR_REV" ]; then
+  if [ -z "$LAST_REVISION" ]; then
+    PATCH_NB=$(git rev-list -n 999999999 --grep='^fix-' -i --count HEAD)
+  else
+    PATCH_NB=$(git rev-list -n 999999999 --grep='^fix-' -i --count ${LAST_REVISION}..HEAD)
+  fi
+else
+  PATCH_NB=$(git rev-list -n 999999999 --grep='^fix-' -i --count ${MINOR_REV}..HEAD)
+fi
+# Compute patch version
+if [ -z "$LAST_VERSION_PATCH" ] || ! [ "$MAJOR_NB" = "0" ] || ! [ "$MINOR_NB" = "0" ]; then
+  PATCH_VERSION=$PATCH_NB
+else
+  PATCH_VERSION=$(expr $LAST_VERSION_PATCH + $PATCH_NB)
+fi
 
-# Update version file
-rm -f $FILENAME
-echo "${MAJOR}.${MINOR}.${PATCH}.${TOTAL}" >> $FILENAME
-echo "${MAJOR}.${MINOR}.${PATCH}.${TOTAL}"
+# Find total number of commits
+if [ -z "$LAST_REVISION" ]; then
+  TOTAL_NB=$(git rev-list -n 999999999 --count HEAD)
+else
+  TOTAL_NB=$(git rev-list -n 999999999 --count ${LAST_REVISION}..HEAD)
+fi
+# Compute total commits
+if [ -z "$LAST_VERSION_TOTAL" ]; then
+  TOTAL_COMMITS=$TOTAL_NB
+else
+  TOTAL_COMMITS=$(expr $LAST_VERSION_TOTAL + $TOTAL_NB)
+fi
+
+
+# -- GENERATE VERSION FILE -- --------------------------------------------------
+
+if [ -e "$VERSION_FILENAME" ]; then
+  rm -f "${VERSION_FILENAME}"
+fi
+VERSION_STRING="${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}.${TOTAL_COMMITS}"
+printf "# ${VERSION_STRING}\nset(CWORK_BUILD_VERSION ${VERSION_STRING})\n" > "${VERSION_FILENAME}"
+echo "> new version: ${VERSION_STRING}"
 
 exit 0
